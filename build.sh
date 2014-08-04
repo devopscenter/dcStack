073bc3b1f76130a1d0be03155690815413c0a0a7
josh <josh@devop.center>
2014-08-04 19:58:22 +0000

    adding worker

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 docker build -rm -t "devopscenter/web" web
-docker build -t "devopscenter/db" db
-#docker build -t "devopscenter/worker" worker
+docker build -rm -t "devopscenter/db" db
+docker build -rm -t "devopscenter/worker" worker
