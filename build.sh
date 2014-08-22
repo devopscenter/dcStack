5e4a4ebb6e4f5c8e28672b40ed4eb0f0399fd303
josh <josh@devops.center>
2014-08-22 18:56:09 +0000

    replace string

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 source VERSION
 echo "Version=${devops_version}"
+
+find . -name "Dockerfile" -type f -exec sed -i "s/devops_version/$devops_version/g" {} \;
+#find /some/folder -type f -exec d -i 's/old_text/new_text/g' {} \;
 docker build -rm -t "devopscenter/python:${devops_version}" python
 docker build -rm -t "devopscenter/web_apache:${devops_version}" web/apache
 docker build -rm -t "devopscenter/db_postgres:${devops_version}" db/postgres
