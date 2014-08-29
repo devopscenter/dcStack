282c11951abd92762887b219bde8cca2a7b6c38e
josh <josh@devops.center>
2014-08-29 15:56:28 +0000

    updates and 0.2.0

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ find . -name "Dockerfile" -type f -exec sed -i -e "s/devops_version/$devops_vers
 
 #build containers
 docker build -rm -t "devopscenter/python:${devops_version}" python
-docker build -rm -t "devopscenter/web_apache:${devops_version}" web/python-apache
-docker build -rm -t "devopscenter/web_apache:${devops_version}" web/python-apache-pgpool
-docker build -rm -t "devopscenter/web_apache:${devops_version}" web/python-apache-pgpool-redis
+docker build -rm -t "devopscenter/python-apache:${devops_version}" web/python-apache
+docker build -rm -t "devopscenter/python-apache-pgpool:${devops_version}" web/python-apache-pgpool
+docker build -rm -t "devopscenter/python-apache-pgpool-redis:${devops_version}" web/python-apache-pgpool-redis
 docker build -rm -t "devopscenter/db_postgres:${devops_version}" db/postgres
 docker build -rm -t "devopscenter/db_postgres-perf-analysis:${devops_version}" db/postgres-performance-analysis
 docker build -rm -t "devopscenter/worker_django-rq:${devops_version}" worker/django-rq
