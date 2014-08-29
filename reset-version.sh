b682f8803a25fe6a59d3366918efd2309fd95fec
josh <josh@devops.center>
2014-08-29 10:37:25 +0200

    update variable

--- reset-version.sh
+++ reset-version.sh
@@ -3,4 +3,4 @@
 source VERSION
 echo "Version=${devops_version}"
 
-find . -name "Dockerfile" -type f -exec sed -i "s/$devops_version/devops_version/g" {} \;
+find . -name "Dockerfile" -type f -exec sed -i -e "s/$devops_version/devops_version/g" {} \;
