1ee56458e1b085c4f4941d104f4830e27e0b9952
josh <josh@devops.center>
2014-08-29 17:09:52 +0000

    updating push adding python link update cmd to use init.d

--- push.sh
+++ push.sh
@@ -3,8 +3,16 @@
 source VERSION
 echo "Version=${devops_version}"
 sudo docker push  "devopscenter/python:${devops_version}" 
-sudo docker push  "devopscenter/web_apache:${devops_version}"
+sudo docker push  "devopscenter/python-apache:${devops_version}"
+sudo docker push  "devopscenter/python-apache-pgpool:${devops_version}"
+sudo docker push  "devopscenter/python-apache-pgpool-redis:${devops_version}"
 sudo docker push  "devopscenter/db_postgres:${devops_version}" 
 sudo docker push  "devopscenter/db_postgres-perf-analysis:${devops_version}" 
 sudo docker push  "devopscenter/worker_django-rq:${devops_version}" 
 sudo docker push  "devopscenter/worker_celery:${devops_version}" 
+
+sudo docker push  "devopscenter/0099ff.web:${devops_version}"
+sudo docker push  "devopscenter/0099ff.worker:${devops_version}"
+
+sudo docker push  "devopscenter/66ccff.web:${devops_version}"
+
