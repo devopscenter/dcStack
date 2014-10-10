16d34b54e77c802494a552e11d111677416f8808
josh <josh@devops.center>
2014-10-10 19:54:55 +0000

    adding license

---
+++ copyright.sh
@@ -0,0 +1,7 @@
+copyright-header --add-path . \
+                 --license ASL2 \
+                 --copyright-holder 'devops.center' \
+                 --copyright-software 'Docker Stack' \
+                 --copyright-software-description "Docker stack to manage infrastructures" \
+                 --copyright-year 2014 \
+                 --output-dir .
