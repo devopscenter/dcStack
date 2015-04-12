a40b2de68ce2aec4b1236731ba0338102601afaf
josh <josh@devops.center>
2015-04-12 01:41:08 +0000

    add django health check remove unused files

--- update-docker-ubuntu.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y lxc-docker
-curl -L https://github.com/docker/fig/releases/download/1.0.0/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
