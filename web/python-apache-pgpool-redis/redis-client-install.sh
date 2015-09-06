b857d370cd9f813a4dc8e6ed20292557d09546ec
josh <josh@devops.center>
2015-09-06 10:21:15 +0200

    redis client

--- web/python-apache-pgpool-redis/redis-client-install.sh
+++ web/python-apache-pgpool-redis/redis-client-install.sh
@@ -2,7 +2,7 @@
 
 #Need redis-cli and libs
 pushd /tmp
-wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
+wget --quiet http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
 tar xvzf redis-$REDIS_VERSION.tar.gz
 pushd /redis-$REDIS_VERSION 
 make -j 3 && sudo make install
