38e14730b4c01d91c48f14b51c652e1bdca5d2a1
josh <josh@devops.center>
2015-09-06 11:03:31 +0200

    fix pushd

--- web/python-apache-pgpool-redis/redis-client-install.sh
+++ web/python-apache-pgpool-redis/redis-client-install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 #Need redis-cli and libs
 pushd /tmp
 wget --quiet http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
 tar xvzf redis-$REDIS_VERSION.tar.gz
-pushd /redis-$REDIS_VERSION 
+pushd redis-$REDIS_VERSION 
 make --silent -j 3 && sudo make --silent install
 
