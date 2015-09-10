c8100804438f5a11e8d0e87ca7a996d3df9a8270
josh <josh@devops.center>
2015-09-10 13:40:37 -0700

    already running as postgres user

--- db/postgres/hstore.sh
+++ db/postgres/hstore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "CREATE EXTENSION hstore;" | \
-    su postgres -c "/usr/lib/postgresql/9.4/bin/postgres --single \
+    /usr/lib/postgresql/9.4/bin/postgres --single \
     -D /var/lib/postgresql/9.4/main \
     -c config_file=$POSTGRES_CONF \
-    template1"
+    template1
