084a1c39c62cb62b73c1e91ab0e81e5e1b5d5b12
josh <josh@devops.center>
2015-09-10 11:18:01 -0700

    apt-fast

--- db/redis/redisinstall.sh
+++ db/redis/redisinstall.sh
@@ -3,6 +3,14 @@
 export REDIS_VERSION=3.0.3
 export REDIS_DOWNLOAD_URL=http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
 
+sudo apt-get update && apt-get -y install python-software-properties software-properties-common && \
+        sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
+            sudo apt-get update
+
+sudo add-apt-repository ppa:saiarcot895/myppa && \
+        sudo apt-get update && \
+            sudo apt-get -y install apt-fast
+
 sudo groupadd -r redis && sudo useradd -r -g redis redis
 
 sudo apt-fast -qq update && apt-fast -qq install -y --no-install-recommends \
