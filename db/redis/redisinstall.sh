ac97ca2222f97524694aaba3f7f8295042201638
josh <josh@devops.center>
2015-09-10 12:55:47 -0700

    fix commands

--- db/redis/redisinstall.sh
+++ db/redis/redisinstall.sh
@@ -20,21 +20,21 @@ sudo apt-fast -qq update && apt-fast -qq install -y --no-install-recommends \
 
 sudo gpg --keyserver pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 
-buildDeps='gcc libc6-dev make' \
-    && set -x \
-    && sudo apt-fast -qq update && sudo apt-fast -qq install -y $buildDeps --no-install-recommends \
-    && sudo rm -rf /var/lib/apt/lists/* \
-    && sudo mkdir -p /usr/src/redis \
-    && pushd /tmp \
-    && curl -sSL "$REDIS_DOWNLOAD_URL" -o redis.tar.gz \
-    && echo "$REDIS_DOWNLOAD_SHA1 *redis.tar.gz" | sha1sum -c - \
-    && tar -xzf redis.tar.gz -C /usr/src/redis --strip-components=1 \
-    && rm redis.tar.gz \
-    && pushd /usr/src/redis
-    && make --silent \
-    && sudo make --silent install \
-    && rm -rf /usr/src/redis \
-    && apt-get purge -y --auto-remove $buildDeps
+buildDeps='gcc libc6-dev make'
+set -x \
+&& sudo apt-fast -qq update && sudo apt-fast -qq install -y $buildDeps --no-install-recommends \
+&& sudo rm -rf /var/lib/apt/lists/* \
+&& sudo mkdir -p /usr/src/redis \
+&& pushd /tmp \
+&& curl -sSL "$REDIS_DOWNLOAD_URL" -o redis.tar.gz \
+&& echo "$REDIS_DOWNLOAD_SHA1 *redis.tar.gz" | sha1sum -c - \
+&& tar -xzf redis.tar.gz -C /usr/src/redis --strip-components=1 \
+&& rm redis.tar.gz \
+&& pushd /usr/src/redis
+&& make --silent \
+&& sudo make --silent install \
+&& rm -rf /usr/src/redis \
+&& apt-get purge -y --auto-remove $buildDeps
 
 sudo mkdir -p /etc/redis
 sudo curl -sSL https://raw.githubusercontent.com/antirez/redis/$REDIS_VERSION/redis.conf -o /etc/redis/redis.conf
