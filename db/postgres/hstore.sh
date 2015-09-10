73c9dd26655937a257c86948957b80e81678da39
josh <josh@devops.center>
2015-09-10 13:30:05 -0700

    fix hstore

--- db/postgres/hstore.sh
+++ db/postgres/hstore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "CREATE EXTENSION hstore;" | \
-    /bin/bash -c "/usr/lib/postgresql/9.4/bin/postgres --single \
+    su postgres -c "/usr/lib/postgresql/9.4/bin/postgres --single \
     -D /var/lib/postgresql/9.4/main \
     -c config_file=$POSTGRES_CONF \
     template1"
