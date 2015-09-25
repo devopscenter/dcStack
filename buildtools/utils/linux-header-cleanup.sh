6dec4f6f6f917c6914317e07ff6d6673b597729b
josh <josh@devops.center>
2015-09-25 01:35:26 -0700

    htop

--- buildtools/utils/linux-header-cleanup.sh
+++ buildtools/utils/linux-header-cleanup.sh
@@ -3,6 +3,7 @@
 #http://askubuntu.com/questions/304810/dependency-problems-prevent-configuration-of-linux-headers-virtual
 
 sudo apt-get remove linux-headers-virtual linux-virtual  
-sudo apt-get install linux-headers-virtual linux-virtual
+sudo apt-get -y autoremove
+sudo apt-get install -y linux-headers-virtual linux-virtual
 sudo apt-get -f install
 sudo apt-get autoremove
