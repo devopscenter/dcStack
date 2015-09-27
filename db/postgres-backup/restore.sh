86e45b986563efae9d6c12e9eab889e41cc8be3f
josh <josh@devops.center>
2015-09-26 21:59:30 -0700

    script updates

--- db/postgres-backup/restore.sh
+++ db/postgres-backup/restore.sh
@@ -3,7 +3,8 @@
 DATABASE=$1
 S3BUCKET=$2
 
-s3cmd ls s3://$S3BUCKET | grep `date +%F` | grep '$DATABASE.sql.gz'
+#Latest first
+FILETODOWNLOAD=$(s3cmd ls s3://$S3BUCKET | grep `date +%F` | grep "$DATABASE".sql.gz | sort -r -k1,2 | head  -1 | awk '{print $4}')
 
 s3cmd get $FILETODOWNLOAD $DATABASE.download
 
