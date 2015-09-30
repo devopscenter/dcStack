063468b31c7dc781a533d383af3db5117093bee5
josh <josh@devops.center>
2015-09-30 05:03:48 -0700

    yes

--- buildtools/jenkins/jenkins.sh
+++ buildtools/jenkins/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -evx
 
 sudo apt-get -qq update && apt-get -qq -y install python-software-properties software-properties-common && \
-        sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
+        sudo add-apt-repository -y "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
             sudo apt-get -qq update
 
 sudo add-apt-repository ppa:saiarcot895/myppa && \
@@ -17,7 +17,7 @@ sudo apt-fast -qq update
 sudo apt-fast -qq -y install jenkins
 
 #echo "JENKINS_HOME=/media/data/jenkins" | sudo tee -a /etc/environment
-echo "JENKINS_HOME=/media/data/jenkins" | sudo tee -a/etc/default/jenkins
+echo "JENKINS_HOME=/media/data/jenkins" | sudo tee -a /etc/default/jenkins
 
 sudo mkdir -p /media/data/jenkins
 sudo chown -R jenkins:jenkins /media/data/jenkins
