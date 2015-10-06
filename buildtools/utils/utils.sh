fa317add19bc265bc2642a851ef57480151077ac
josh <josh@devops.center>
2015-10-06 02:47:28 -0700

    security updates

--- buildtools/utils/utils.sh
+++ buildtools/utils/utils.sh
@@ -5,3 +5,6 @@ sudo apt-fast -y install ncdu ntp fail2ban htop
 #sudo add-apt-repository -y ppa:tualatrix/ppa
 #sudo apt-fast update
 #sudo apt-fast -y install ubuntu-tweak
+
+sudo apt-fast -y install unattended-upgrades
+sudo dpkg-reconfigure --priority=low unattended-upgrades
