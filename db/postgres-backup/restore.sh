a032100f9ddb88826981c607805709b68446d026
josh <josh@devops.center>
2015-10-15 01:15:57 -0700

    restore script

--- db/postgres-backup/restore.sh
+++ db/postgres-backup/restore.sh
@@ -3,10 +3,13 @@
 DATABASE=$1
 S3BUCKET=$2
 
+YEAR=`date +%Y`
+MONTH=`date +%m`
+DAY=`date +%d`
 #Latest first
-FILETODOWNLOAD=$(s3cmd ls s3://$S3BUCKET | grep `date +%F` | grep "$DATABASE".sql.gz | sort -r -k1,2 | head  -1 | awk '{print $4}')
+FILETODOWNLOAD=$(s3cmd ls s3://$S3BUCKET/$YEAR/$MONTH/ | grep `date +%F -d "yesterday"` | grep "$DATABASE".sql.gz | sort -r -k1,2 | head  -1 | awk '{print $4}')
 
-s3cmd get $FILETODOWNLOAD $DATABASE.download
+s3cmd --force get $FILETODOWNLOAD $DATABASE.download
 
 psql -U postgres postgres -c "alter database $DATABASE rename to $DATABASE_backup"
 psql -U postgres postgres -c "create database $DATABASE"
