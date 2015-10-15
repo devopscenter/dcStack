e89b3e03fbb7040da66ed4609d20dc76a752b061
josh <josh@devops.center>
2015-10-15 01:23:18 -0700

    roles

--- db/postgres-backup/restore.sh
+++ db/postgres-backup/restore.sh
@@ -6,12 +6,18 @@ S3BUCKET=$2
 YEAR=`date +%Y`
 MONTH=`date +%m`
 DAY=`date +%d`
+
+
+ROLESTODOWNLOAD=$(s3cmd ls s3://$S3BUCKET/$YEAR/$MONTH/ | grep `date +%F -d "yesterday"` | grep roles | sort -r -k1,2 | head  -1 | awk '{print $4}')
+s3cmd --force get $ROLESTODOWNLOAD roles.download
+psql -f roles.download  -U postgres
+
 #Latest first
 FILETODOWNLOAD=$(s3cmd ls s3://$S3BUCKET/$YEAR/$MONTH/ | grep `date +%F -d "yesterday"` | grep "$DATABASE".sql.gz | sort -r -k1,2 | head  -1 | awk '{print $4}')
 
 s3cmd --force get $FILETODOWNLOAD $DATABASE.download
 
-psql -U postgres postgres -c "alter database $DATABASE rename to $DATABASE_backup"
+psql -U postgres postgres -c "alter database $DATABASE rename to ${DATABASE}_backup"
 psql -U postgres postgres -c "create database $DATABASE"
 
 pg_restore -e -U postgres -Fc --dbname=$DATABASE $DATABASE.download
