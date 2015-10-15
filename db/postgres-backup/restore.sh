15746be2c35441ff61243a79b1a9a55b46b3b69b
josh <josh@devops.center>
2015-10-15 01:38:02 -0700

    backupdir

--- db/postgres-backup/restore.sh
+++ db/postgres-backup/restore.sh
@@ -2,6 +2,7 @@
 
 DATABASE=$1
 S3BUCKET=$2
+BACKUPDIR=/media/data/postgres/backup
 
 YEAR=`date +%Y`
 MONTH=`date +%m`
@@ -9,15 +10,16 @@ DAY=`date +%d`
 
 
 ROLESTODOWNLOAD=$(s3cmd ls s3://$S3BUCKET/$YEAR/$MONTH/ | grep `date +%F -d "yesterday"` | grep roles | sort -r -k1,2 | head  -1 | awk '{print $4}')
-s3cmd --force get $ROLESTODOWNLOAD roles.download
-psql -f roles.download  -U postgres
+s3cmd --force get $ROLESTODOWNLOAD ${BACKUPDIR}/roles.download
+psql -f ${BACKUPDIR}/roles.download  -U postgres
 
 #Latest first
 FILETODOWNLOAD=$(s3cmd ls s3://$S3BUCKET/$YEAR/$MONTH/ | grep `date +%F -d "yesterday"` | grep "$DATABASE".sql.gz | sort -r -k1,2 | head  -1 | awk '{print $4}')
 
-s3cmd --force get $FILETODOWNLOAD $DATABASE.download
+s3cmd --force get $FILETODOWNLOAD ${BACKUPDIR}/$DATABASE.download
 
+dropdb rmsa_backup --if-exists -U postgres
 psql -U postgres postgres -c "alter database $DATABASE rename to ${DATABASE}_backup"
 psql -U postgres postgres -c "create database $DATABASE"
 
-pg_restore -e -U postgres -Fc --dbname=$DATABASE $DATABASE.download
+pg_restore -e -U postgres -Fc --dbname=$DATABASE ${BACKUPDIR}/$DATABASE.download
