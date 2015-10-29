d12440302b9f9c8bef3f56ba1bfb11b7d7a9a0b2
Bob Lozano <boblozano@gmail.com>
2015-10-28 19:08:41 -0500

    initial aws mount config

--- db/postgres/mount.sh
+++ db/postgres/mount.sh
@@ -1,6 +1,24 @@
-#!/bin/bash -ev
+#!/bin/bash
 
-MOUNTPATH=/dev/xvdi
+MOUNTPATH=/dev/xvdb
+DIRECTORY=/media/data/postgres/db
+
+echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
+sudo mkfs -t ext4 ${MOUNTPATH}
+sudo mkdir -p ${DIRECTORY}
+sudo mount ${MOUNTPATH} ${DIRECTORY}
+
+
+MOUNTPATH=/dev/xvdg
+DIRECTORY=/media/data/postgres/xlog
+
+echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
+sudo mkfs -t ext4 ${MOUNTPATH}
+sudo mkdir -p ${DIRECTORY}
+sudo mount ${MOUNTPATH} ${DIRECTORY}
+
+
+MOUNTPATH=/dev/xvdh
 DIRECTORY=/media/data/postgres/backup
 
 echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
