07e09791174b0f50d28e8760f2a370c76f6d5d81
Bob Lozano <boblozano@gmail.com>
2015-11-18 19:33:16 -0600

    enable papertrail logging

---
+++ logging/enable-logging.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# note that this is only run on an instance, not within a container.
+
+sudo pip install supervisor-logging
+
+export SYSLOG_SERVER = 'logs2.papertrailapp.com'
+export SYSLOG_PORT = 48809
+export SYSLOG_PROTO = 'tcp'
