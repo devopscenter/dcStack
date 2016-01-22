787f3aeacdd10741dd07c20b1e1cc07c01ca07bb
Trey Dembski <trey@devops.center>
2016-01-22 12:06:24 -0600

    add logging to postgres creation script

--- buildtools/utils/base-utils.sh
+++ buildtools/utils/base-utils.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get -qq update && sudo apt-get -qq -y install python-software-properties software-properties-common && \
-      sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
-          sudo apt-get -qq update
+    sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
+    sudo apt-get -qq update
 
 sudo add-apt-repository -y ppa:saiarcot895/myppa && \
-      sudo apt-get -qq update && \
-          sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install apt-fast
+    sudo apt-get -qq update && \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install apt-fast
 
 sudo apt-fast -qq -y install git python-dev python-pip wget sudo vim supervisor
 
@@ -15,5 +15,5 @@ sudo apt-fast -y install ncdu ntp fail2ban htop
 #sudo apt-fast update
 #sudo apt-fast -y install ubuntu-tweak
 
-echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
+echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | sudo debconf-set-selections
 sudo apt-fast -y install unattended-upgrades
