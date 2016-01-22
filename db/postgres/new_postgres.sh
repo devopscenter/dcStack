85944a496e3f25eec349bd15525888a1f5c3459e
Trey Dembski <trey@devops.center>
2016-01-22 14:24:52 -0600

    more changes re: sudo and echo

--- db/postgres/new_postgres.sh
+++ db/postgres/new_postgres.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 PRIVATE_IP=$1
 
+# install standard packages/utilities
+cd ../../buildtools/utils/
+sudo ./base-utils
+
 # add private IP to /etc/hosts
 if ! (grep -q "^${PRIVATE_IP}\b.*\bpostgresmaster_1\b" /etc/hosts); then
   echo "${PRIVATE_IP} postgresmaster_1" | tee -a /etc/hosts > /dev/null
 fi
 
 # mount volumes and remove instance attached store from /mnt
-./docker-stack/db/postgres/mount.sh
+cd ../../db/postgres/
+sudo ./mount.sh
 sudo sed '/\/dev\/xvdb[[:blank:]]\/mnt/d' /etc/fstab
 
 # install postgres and other tasks
-./docker-stack/db/postgres/postgres.sh
+sudo ./postgres.sh
 
 # install pgtune and restart postgres with new config
 if [[ ! -d /media/data/tmp ]]; then
@@ -20,7 +25,11 @@ fi
 sudo chown ubuntu:ubuntu /media/data/tmp
 cd /media/data/tmp
 
-sudo apt-get install pgtune
+sudo apt-fast -y install pgtune
 sudo pgtune -i /media/data/postgres/db/pgdata/postgresql.conf -o postgresql.conf.pgtune
 sudo cp postgresql.conf.pgtune /media/data/postgres/db/pgdata/postgresql.conf
 sudo supervisorctl restart postgres
+
+# enable logging
+cd ~/docker-stack/logging/
+./enable-pg-logging.sh
