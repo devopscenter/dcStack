b7465baf370921609c1edaec00c350dd3e92b228
Trey Dembski <trey@devops.center>
2016-02-04 14:07:43 -0600

    more changes to continue using s3cmd for now

--- db/postgres-backup/enable-backup.sh
+++ db/postgres-backup/enable-backup.sh
@@ -12,12 +12,12 @@ sudo pip install s3cmd
 # copy over backup script and config, update s3 bucket in config.
 cd ~/docker-stack/db/postgres-backup/
 sudo cp pg_backup.config /media/data/postgres/backup/pg_backup.config
-sudo cp pg_backup_rotated_new.sh /media/data/postgres/backup/pg_backup_rotated_new.sh
-sudo chmod 0755 /media/data/postgres/backup/pg_backup_rotated_new.sh
+sudo cp pg_backup_rotated.sh /media/data/postgres/backup/pg_backup_rotated.sh
+sudo chmod 0755 /media/data/postgres/backup/pg_backup_rotated.sh
 sudo sed "s/^BUCKET_NAME=.*/BUCKET_NAME=${S3_BUCKET}/" pg_backup.config
 
 # add cron job to run the backup daily
 #sudo (crontab -u postgres -l 2>/dev/null; echo "$1 $2 $3 $4 $5 /path/to/job -with args") | crontab -u postgres -
-(sudo crontab -u postgres -l 2>/dev/null; echo "01 04  *   *   *     /media/data/postgres/backup/pg_backup_rotated_new.sh -c /media/data/postgres/backup/pg_backup.config") | sudo crontab -u postgres -
+(sudo crontab -u postgres -l 2>/dev/null; echo "01 04  *   *   *     /media/data/postgres/backup/pg_backup_rotated.sh -c /media/data/postgres/backup/pg_backup.config") | sudo crontab -u postgres -
 
 # jenkins setup
