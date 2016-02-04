f986c5e72d482bb77b8c031c5663dc51370af079
Trey Dembski <trey@devops.center>
2016-02-04 12:08:06 -0600

    fixed variable name, added awscli equivalents to s3cmd commands as comments

--- db/postgres-restore/restore-backup.sh
+++ db/postgres-restore/restore-backup.sh
@@ -41,22 +41,27 @@ sudo pip install s3cmd
 # list contents of s3 bucket
 if ! [[ -z "$LIST" ]]; then
   echo "S3 backups, with most recent listed last"
-  s3cmd ls -r s3://"${S3BUCKET}"/|grep "${DBNAME}".sql.gz|awk -F/ '{print $6}'|sort -V
+  s3cmd ls -r s3://"${S3_BUCKET}"/|grep "${DBNAME}".sql.gz|awk -F/ '{print $6}'|sort -V
+#  aws s3 ls --recursive s3://"${S3_BUCKET}"/|grep "${DBNAME}".sql.gz|awk -F/ '{print $3}'|sort -V
   exit 1
 fi
 
 # if the backup file isn't specified, grab the most recent
 if [[ -z "$BACKUPFILE" ]]; then
-  S3_FILE=$(s3cmd ls -r s3://"${S3BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk '{print $4}')
-  BACKUPFILE=$(s3cmd ls -r s3://"${S3BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk -F/ '{print $6}')
+  S3_FILE=$(s3cmd ls -r s3://"${S3_BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk '{print $4}')
+#  S3_FILE="s3://${S3_BUCKET}/$(aws s3 ls --recursive s3://"${S3_BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk '{print $4}')"
+  BACKUPFILE=$(s3cmd ls -r s3://"${S3_BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk -F/ '{print $6}')
+#  BACKUPFILE=$(aws s3 ls --recursive s3://"${S3_BUCKET}"/|grep "${DBNAME}.sql.gz"|sort -r -k1,2|head -1|awk -F/ '{print $3}')
 else
-  S3_FILE=$(s3cmd ls -r s3://"${S3BUCKET}"/|grep "${BACKUPFILE}"|awk '{print $4}')
+  S3_FILE=$(s3cmd ls -r s3://"${S3_BUCKET}"/|grep "${BACKUPFILE}"|awk '{print $4}')
+#  S3_FILE="s3://${S3_BUCKET}/$(aws s3 ls --recursive s3://"${S3_BUCKET}"/|grep "${BACKUPFILE}"|awk '{print $4}')"
 fi
 S3_YEAR=$(echo "$S3_FILE"|awk -F/ '{print $4}')
 S3_MONTH=$(echo "$S3_FILE"|awk -F/ '{print $5}')
 
 # download backup, drop db if it already exists, create it, restore into it
-sudo s3cmd --force get "s3://${S3BUCKET}/${S3_YEAR}/${S3_MONTH}/${BACKUPFILE}" "${BACKUPDIR}/${BACKUPFILE}.download"
+sudo s3cmd --force get "s3://${S3_BUCKET}/${S3_YEAR}/${S3_MONTH}/${BACKUPFILE}" "${BACKUPDIR}/${BACKUPFILE}.download"
+#sudo aws s3 cp "s3://${S3_BUCKET}/${S3_YEAR}/${S3_MONTH}/${BACKUPFILE}" "${BACKUPDIR}/${BACKUPFILE}.download"
 dropdb "${DBNAME}" --if-exists -U postgres
 psql -U postgres postgres -c "create database $DBNAME"
 echo "Postgresql restore started at " && date
