3a87d0f380925ea34c772bcd4f2e83dc1827b392
Trey Dembski <trey@devops.center>
2016-02-12 14:02:19 -0600

    reuse mount code with function

--- db/postgres/mount.sh
+++ db/postgres/mount.sh
@@ -5,43 +5,47 @@
 # enable us to modify /etc/fstab
 sudo chmod o+w /etc/fstab
 
-# make use of attached db volume if it exists, otherwise use instance-attached ssd.
+function mount-volume
+{
+  MOUNTPATH="$1"
+  DIRECTORY="$2"
 
+  # add primary partition if it doesn't exist
+  if ! (sudo parted "$MOUNTPATH" print|grep -q ext4); then
+    echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
+  fi
+
+  # create ext4 filesystem if it doesn't exist
+  if ! (sudo file -Ls "$MOUNTPATH"|grep -q ext4); then
+    if (sudo mount -t ext4|grep -q "$MOUNTPATH"); then
+      sudo umount "$MOUNTPATH"
+    fi
+    sudo mkfs -t ext4 "$MOUNTPATH"
+  fi
+
+  # mount at the target directory
+  sudo mkdir -p ${DIRECTORY}
+  if ! (mount|grep -q "^${MOUNTPATH}\b[[:blank:]]*\bon\b[[:blank:]]*${DIRECTORY}\b"); then
+    sudo mount ${MOUNTPATH} ${DIRECTORY}
+  fi
+
+  # add to fstab to mount on boot
+  FSTAB_LINE="${MOUNTPATH}   ${DIRECTORY}     auto    defaults,nobootwait,comment=cloudconfig 0       2"
+  if ! (grep -q "$FSTAB_LINE" /etc/fstab); then
+    echo "$FSTAB_LINE" | sudo tee -a /etc/fstab
+  fi
+}
+
+# make use of attached db volume if it exists, otherwise use instance-attached ssd
 if [ -b /dev/xvdg ]
     then
-        MOUNTPATH=/dev/xvdg
+        DB_DEV=/dev/xvdg
     else
-        MOUNTPATH=/dev/xvdb
+        DB_DEV=/dev/xvdb
 fi
-DIRECTORY=/media/data/postgres/db
-
-echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
-sudo mkfs -t ext4 ${MOUNTPATH}
-sudo mkdir -p ${DIRECTORY}
-sudo mount ${MOUNTPATH} ${DIRECTORY}
-sudo echo "${MOUNTPATH}   ${DIRECTORY}     auto    defaults,nobootwait,comment=cloudconfig 0       2" >> /etc/fstab
-
-
-MOUNTPATH=/dev/xvdh
-DIRECTORY=/media/data/postgres/xlog
-
-echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
-sudo mkfs -t ext4 ${MOUNTPATH}
-sudo mkdir -p ${DIRECTORY}
-sudo mount ${MOUNTPATH} ${DIRECTORY}
-sudo echo "${MOUNTPATH}   ${DIRECTORY}     auto    defaults,nobootwait,comment=cloudconfig 0       2" >> /etc/fstab
-
-
-
-MOUNTPATH=/dev/xvdi
-DIRECTORY=/media/data/postgres/backup
-
-echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
-sudo mkfs -t ext4 ${MOUNTPATH}
-sudo mkdir -p ${DIRECTORY}
-sudo mount ${MOUNTPATH} ${DIRECTORY}
-sudo echo "${MOUNTPATH}   ${DIRECTORY}     auto    defaults,nobootwait,comment=cloudconfig 0       2" >> /etc/fstab
+mount-volume "$DB_DEV" "/media/data/postgres/db"
+mount-volume "/dev/xvdh" "/media/data/postgres/xlog"
+mount-volume "/dev/xvdi" "/media/data/postgres/backup"
 
 # close up /etc/fstab again
-
 sudo chmod o-w /etc/fstab
