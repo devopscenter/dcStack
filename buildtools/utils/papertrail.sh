2bd9c996673dfd7cd90d226499261264cd609a7c
devopsscion <devopsscion@devopsscion.com>
2016-02-14 08:54:02 -0800

    base image

---
+++ buildtools/utils/papertrail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ev
+
+sudo apt-fast -y install rsyslog-gnutls
+sudo curl -o /etc/papertrail-bundle.pem https://papertrailapp.com/tools/papertrail-bundle.pem
+sudo cat papertrail.conf | sudo tee --append /etc/rsyslog.conf
