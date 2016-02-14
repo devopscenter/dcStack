b6032625a331f56ab6877a4deabb8f0e7a5ac11c
devopsscion <devopsscion@devopsscion.com>
2016-02-13 23:19:22 -0800

    configs

--- db/postgres/config.sh
+++ db/postgres/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
-VPC_CIDR=$1
-DATABASE=$2
-VPC_CIDR_ESCAPED=$(echo ${VPC_CIDR}|awk -F/ '{print $1 "\\""/" $2}')
+#VPC_CIDR=$1
+#DATABASE=$2
+#VPC_CIDR_ESCAPED=$(echo ${VPC_CIDR}|awk -F/ '{print $1 "\\""/" $2}')
 
 . ./postgresenv.sh
 
@@ -11,10 +11,10 @@ VPC_CIDR_ESCAPED=$(echo ${VPC_CIDR}|awk -F/ '{print $1 "\\""/" $2}')
 cat ./conf/hba.conf >> "${POSTGRESDBDIR}"/pg_hba.conf
 
 # replace VPC_CIDR and DATABASE in pg_hba.conf and uncomment lines if necessary
-sed -i "s/<VPC_CIDR>/${VPC_CIDR_ESCAPED}/g" "${POSTGRESDBDIR}"/pg_hba.conf
-sed -i "s/\(#\)\(.*${VPC_CIDR_ESCAPED}.*\)/\2/g" "${POSTGRESDBDIR}"/pg_hba.conf
-sed -i "s/<DATABASE>/${DATABASE}/g" "${POSTGRESDBDIR}"/pg_hba.conf
-sed -i "s/\(#\)\(.*${DATABASE}.*\)/\2/g" "${POSTGRESDBDIR}"/pg_hba.conf
+#sed -i "s/<VPC_CIDR>/${VPC_CIDR_ESCAPED}/g" "${POSTGRESDBDIR}"/pg_hba.conf
+#sed -i "s/\(#\)\(.*${VPC_CIDR_ESCAPED}.*\)/\2/g" "${POSTGRESDBDIR}"/pg_hba.conf
+#sed -i "s/<DATABASE>/${DATABASE}/g" "${POSTGRESDBDIR}"/pg_hba.conf
+#sed -i "s/\(#\)\(.*${DATABASE}.*\)/\2/g" "${POSTGRESDBDIR}"/pg_hba.conf
 
 cat ./conf/pg.conf >> "${POSTGRESDBDIR}"/postgresql.conf
 mkdir -p /var/run/postgresql/postgres-main.pg_stat_tmp
