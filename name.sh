026d9c5cf5e43f5d20a26af94c8a442e7b3795ef
devopsscion <devopsscion@devopsscion.com>
2016-02-13 20:53:02 -0800

    devopsscion

--- name.sh
+++ name.sh
@@ -8,3 +8,4 @@ echo "BaseImage=${baseimageversion}"
 #replace variable devops_version with the VERSION we are building
 find . -name "Dockerfile" -type f -exec sed -i -e "s/devops_version/$devops_version/g" {} \;
 find . -name "Dockerfile" -type f -exec sed -i -e "s~baseimageversion~$baseimageversion~g" {} \;
+find . -name "Dockerfile" -type f -exec sed -i -e "s~FROM devopscenter~FROM devopsscion~g" {} \;
