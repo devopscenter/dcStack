9b3a714ea0cd5bbe0ae64344c31976995c2ee7ee
devopsscion <devopsscion@devopsscion.com>
2016-02-15 07:18:52 -0800

    papertrailqueue

--- logging/papertrail.sh
+++ logging/papertrail.sh
@@ -2,7 +2,7 @@
 
 sudo apt-fast -y install rsyslog-gnutls
 sudo curl -o /etc/papertrail-bundle.pem https://papertrailapp.com/tools/papertrail-bundle.pem
-sudo cp papertrail.conf /etc/rsyslog.d/papertrailqueue.conf
+sudo cp papertrailqueue.conf /etc/rsyslog.d/papertrailqueue.conf
 
 # comment out broken section of this file and restart rsyslog
 # https://bugs.launchpad.net/ubuntu/+source/rsyslog/+bug/830046
