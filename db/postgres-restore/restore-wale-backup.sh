b1e06d0440e53708df9811eca406dff303ea660c
Trey Dembski <trey@devops.center>
2016-02-16 18:22:49 -0600

    add nightly backups for wal-e

--- db/postgres-restore/restore-wale-backup.sh
+++ db/postgres-restore/restore-wale-backup.sh
@@ -4,7 +4,7 @@ BACKUPDIR=/media/data/postgres/backup
 
 function usage
 {
-  echo "usage: ./restore-wale-backup.sh [--backupfile backup-file] [--list] s3base s3suffix aws-hostname"
+  echo "usage: ./restore-wale-backup.sh [--backupfile backup-file] [--recoverytime recovery-time] [--list] s3base s3suffix aws-hostname"
 }
 
 if [[ -z $1 ]]; then
@@ -14,24 +14,27 @@ fi
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --backupfile ) shift
-                   BACKUPFILE=$1
-                   ;;
-    --list )       LIST=1
-                   ;;
-    [!-]* )        if [[ $# -eq 3 ]]; then
-                     S3BASE=$1
-                     SUFFIX=$2
-		     AWS_HOSTNAME=$3
-                     shift; shift;
-                   else
-                     echo "Too many/few of the 3 required parameters."
-                     usage
+    --backupfile )   shift
+                     BACKUPFILE=$1
+                     ;;
+    --recoverytime ) shift
+                     RECOVERYTIME=$1
+                     ;;
+    --list )         LIST=1
+                     ;;
+    [!-]* )          if [[ $# -eq 3 ]]; then
+                       S3BASE=$1
+                       SUFFIX=$2
+		       AWS_HOSTNAME=$3
+                       shift; shift;
+                     else
+                       echo "Too many/few of the 3 required parameters."
+                       usage
+                       exit 1
+                     fi
+                     ;;
+    * )              usage
                      exit 1
-                   fi
-                   ;;
-    * )            usage
-                   exit 1
   esac
   shift
 done
@@ -41,6 +44,8 @@ if ! [[ -z "$LIST" ]]; then
   sudo su -c "wal-e --aws-instance-profile --s3-prefix s3://${S3BASE}-postgres-wale-${SUFFIX}/${AWS_HOSTNAME} backup-list" -s /bin/sh postgres
   exit 1
 fi
+
+sudo supervisorctl stop postgres
 
 # if the backup file isn't specified, use LATEST
 if [[ -z "$BACKUPFILE" ]]; then
@@ -54,5 +59,11 @@ sudo cp --preserve /media/data/postgres/backup/postgresql.conf /media/data/postg
 sudo cp --preserve /media/data/postgres/backup/pg_hba.conf /media/data/postgres/db/pgdata/
 sudo cp --preserve /media/data/postgres/backup/pg_ident.conf /media/data/postgres/db/pgdata/
 
-# start postgres
+# create recovery.conf file
+echo 'restore_command  = '\''wal-e --aws-instance-profile --s3-prefix s3://${S3BASE}-postgres-wale-${SUFFIX}/${AWS_HOSTNAME} wal-fetch "%f" "%p"'\'|sudo tee -a /media/data/postgres/db/pgdata/recovery.conf > /dev/null
+echo 'pause_at_recovery_target = false'|sudo tee -a /media/data/postgres/db/pgdata/recovery.conf > /dev/null
+if [[ -n "$RECOVERYTIME" ]]; then
+  echo "recovery_target_time = '${RECOVERYTIME}'"|sudo tee -a /media/data/postgres/db/pgdata/recovery.conf > /dev/null
+fi
+
 sudo supervisorctl start postgres
