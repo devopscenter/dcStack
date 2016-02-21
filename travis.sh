d747338fd1442853b1574c2c7d96275f5462c950
devopsscion <devopsscion@devopsscion.com>
2016-02-21 10:03:44 -0800

    build all

--- travis.sh
+++ travis.sh
@@ -1,3 +1,26 @@
 #!/bin/bash -ev
 
-python/python.sh
+pushd python
+./python.sh
+popd
+
+pushd web/python-nginx
+./nginx.sh
+popd
+
+pushd web/python-nginx-pgpool
+./pgpool.sh
+popd
+
+pushd web/python-nginx-pgpool-libsodium
+git clone https://github.com/devopsscion/libsodium-jni
+git clone https://github.com/data-luminosity/message
+    pushd libsodium-jni
+    ./dependencies-linux.sh
+    ./build.sh
+    popd
+
+    pushd message
+    pip install -r requirements.txt
+    popd
+popd
