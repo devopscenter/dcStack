454a086ccb6c974c4e714a941afefb3f31580c8b
devopsscion <devopsscion@devopsscion.com>
2016-02-21 16:43:12 -0800

    purge

--- db/postgres/postgres.sh
+++ db/postgres/postgres.sh
@@ -27,7 +27,9 @@ sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_rele
 
 sudo apt-fast -qq update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-fast -y -qq install postgresql-${POSTGRES_VERSION} postgresql-client-${POSTGRES_VERSION} postgresql-server-dev-${POSTGRES_VERSION} libpq5 libpq-dev
+sudo apt-get --purge autoremove postgresql-${POSTGRES_VERSION} postgresql
+
+sudo apt-fast -y -qq install postgresql-${POSTGRES_VERSION} postgresql-client-${POSTGRES_VERSION} postgresql-server-dev-${POSTGRES_VERSION} libpq5 libpq-dev
 
 sudo apt-fast -y -qq install postgresql-contrib-${POSTGRES_VERSION} postgresql-${POSTGRES_VERSION}-postgis-2.2
 
