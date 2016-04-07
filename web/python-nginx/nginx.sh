862688a59ec4ad57b160b76e811c8e3e9ce3334e
Bob Lozano <boblozano@gmail.com>
2016-04-07 18:02:12 -0500

    rsyslogd needs to run as a service so that it can log supervisor events, not under supervisor itself

--- web/python-nginx/nginx.sh
+++ web/python-nginx/nginx.sh
@@ -34,5 +34,3 @@ popd
 sudo cp conf/nginx.conf /usr/local/nginx/conf/nginx.conf
 sudo cp conf/supervisor-nginx.conf /etc/supervisor/conf.d/nginx.conf
 sudo cp conf/supervisor-uwsgi.conf /etc/supervisor/conf.d/uwsgi.conf
-sudo cp conf/supervisor-rsyslogd.conf /etc/supervisor/conf.d/rsyslogd.conf
-
