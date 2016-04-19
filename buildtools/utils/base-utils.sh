c8ed8e196c3375b590a82aa966fbac2d223e6c15
Bob Lozano <boblozano@gmail.com>
2016-04-19 18:25:13 -0500

    eliminate myppa and awscli

--- buildtools/utils/base-utils.sh
+++ buildtools/utils/base-utils.sh
@@ -3,8 +3,8 @@
 #    sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
 #    sudo apt-get -qq update
 
-sudo add-apt-repository -y ppa:saiarcot895/myppa && \
-    sudo apt-get -qq update && \
+#sudo add-apt-repository -y ppa:saiarcot895/myppa && \
+#    sudo apt-get -qq update && \
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install apt-fast
 
 sudo apt-fast -qq -y install git wget sudo vim unzip curl
@@ -15,9 +15,9 @@ sudo apt-fast -y install ncdu ntp fail2ban htop
 #sudo apt-fast update
 #sudo apt-fast -y install ubuntu-tweak
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#unzip awscli-bundle.zip
+#sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | sudo debconf-set-selections
 sudo apt-fast -y install unattended-upgrades
