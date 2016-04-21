abb9defcfc576ec84c36f3558dd600a0f7cc5fba
Bob Lozano <boblozano@gmail.com>
2016-04-21 09:11:28 -0500

    rename postgres supervisor config for consistency

--- db/postgres/supervisorconfig.sh
+++ db/postgres/supervisorconfig.sh
@@ -4,6 +4,6 @@
 
 #SUPERVISORD
 sudo cp ./conf/rsyslogd.conf /etc/supervisor/conf.d/rsyslogd.conf
-sudo cp ./conf/postgres.conf /etc/supervisor/conf.d/postgres.conf
+sudo cp ./conf/supervisor-postgres.conf /etc/supervisor/conf.d/postgres.conf
 sudo cp ./conf/run_postgres.sh /etc/supervisor/conf.d/run_postgres.sh
 
