b83db70eb6f71641aaa0a933358976c32a528381
Bob Lozano <boblozano@gmail.com>
2016-04-25 18:37:49 -0500

    syntax

--- db/postgres/supervisorconfig.sh
+++ db/postgres/supervisorconfig.sh
@@ -7,5 +7,5 @@ PGVERSION=$1
 sudo cp ./conf/rsyslogd.conf /etc/supervisor/conf.d/rsyslogd.conf
 sudo cp ./conf/supervisor-postgres.conf /etc/supervisor/conf.d/postgres.conf
 sudo cp ./conf/run_postgres.sh /etc/supervisor/conf.d/run_postgres.sh
-echo "export POSTGRES_VERSION=${POSTGRES_VERSION"  | sudo tee -a /etc/default/supervisor
+echo "export POSTGRES_VERSION=${POSTGRES_VERSION}"  | sudo tee -a /etc/default/supervisor
 
