c3386cc19675ae476354ad2187c4dcc9c5f73f5b
Bob Lozano <boblozano@gmail.com>
2016-04-25 18:34:46 -0500

    pass postgres version to supervisor, in order to start correctly

--- db/postgres/conf/run_postgres.sh
+++ db/postgres/conf/run_postgres.sh
@@ -9,5 +9,5 @@ else
     install -d -m 2775 -o postgres -g postgres /var/run/postgresql/postgres-main.pg_stat_tmp
 fi
 
-exec su postgres -c '/usr/lib/postgresql/9.5/bin/postgres -D /media/data/postgres/db/pgdata -c config_file=/media/data/postgres/db/pgdata/postgresql.conf'
+exec su postgres -c '/usr/lib/postgresql/${POSTGRES_VERSION}/bin/postgres -D /media/data/postgres/db/pgdata -c config_file=/media/data/postgres/db/pgdata/postgresql.conf'
 
