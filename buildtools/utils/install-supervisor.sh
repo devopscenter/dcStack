e6854c57a281d13a0262780d32c0861832d7ab3e
Bob Lozano <boblozano@gmail.com>
2016-04-28 14:36:26 -0500

    make sure to add symlink to config file all the time

--- buildtools/utils/install-supervisor.sh
+++ buildtools/utils/install-supervisor.sh
@@ -14,17 +14,20 @@ else
   sudo cp ~/docker-stack/buildtools/utils/initd-supervisor /etc/init.d/supervisor
 fi
 
+# Create the config directory structure that the ubuntu-packaged supervisor uses.
 sudo mkdir /etc/supervisor
 sudo mkdir /etc/supervisor/conf.d
 sudo mkdir /var/log/supervisor
 sudo cp ~/docker-stack/buildtools/utils/supervisord.conf /etc/supervisor/
 
+# make a symlink so that the pip-installed supervisor can find the configuration file
+sudo ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
+
 # Now make sure that it starts up upon reboot
 sudo update-rc.d supervisor defaults
 
-# Finally set up symlinks if installed in the custom location
+# Finally set up symlinks if installed in the custom location (using a non-ubuntu python)
 if [[ $1 == "custom" ]]; then
-  sudo ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
   sudo ln -s /usr/local/opt/python/bin/supervisord /usr/bin/supervisord
   sudo ln -s /usr/local/opt/python/bin/supervisorctl /usr/bin/supervisorctl
 fi
