ddd4ade7e1d2dd289ee351a1b974b8e829ca0e27
Bob Lozano <boblozano@gmail.com>
2016-04-29 17:44:22 -0500

    Permit connections from anyone to any db, from WITHIN the VPC

--- db/postgres/mount - old.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ev
-
-MOUNTPATH=/dev/xvdi
-DIRECTORY=/media/data/postgres/backup
-
-echo -e "n\np\n1\n\n\nw" | sudo fdisk ${MOUNTPATH}
-sudo mkfs -t ext4 ${MOUNTPATH}
-sudo mkdir -p ${DIRECTORY}
-sudo mount ${MOUNTPATH} ${DIRECTORY}
