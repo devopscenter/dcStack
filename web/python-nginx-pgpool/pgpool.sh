61d3971cf394fa6422bbf90ed9ab0bff3b6e9606
Bob Lozano <boblozano@gmail.com>
2016-05-20 16:54:22 -0500

    cleanup comment

--- web/python-nginx-pgpool/pgpool.sh
+++ web/python-nginx-pgpool/pgpool.sh
@@ -14,7 +14,7 @@ sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
       sudo apt-key add -
 
 # Add PostgreSQL's repository. It contains the most recent stable release
-#     of PostgreSQL, ``9.4``.
+#     of PostgreSQL, ``9.5``.
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
 
 sudo sudo apt-fast update
