6f6b996cfa172572fbdcd587d8ebcc4d3a2c4f8b
Bob Lozano <boblozano@gmail.com>
2016-05-20 14:42:26 -0500

    clean out spurious rsyslog config from supervisor

--- db/postgres/supervisorconfig.sh
+++ db/postgres/supervisorconfig.sh
@@ -4,7 +4,6 @@ PGVERSION=$1
 . ./postgresenv.sh $PGVERSION
 
 #SUPERVISORD
-sudo cp ./conf/rsyslogd.conf /etc/supervisor/conf.d/rsyslogd.conf
 sudo cp ./conf/supervisor-postgres.conf /etc/supervisor/conf.d/postgres.conf
 sudo cp ./conf/run_postgres.sh /etc/supervisor/conf.d/run_postgres.sh
 echo "export POSTGRES_VERSION=${POSTGRES_VERSION}"  | sudo tee -a /etc/default/supervisor
