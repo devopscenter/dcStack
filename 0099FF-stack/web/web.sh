de9f74cd0293faa2e6871f0c19d9fc9ea2f81b19
Bob Lozano <boblozano@gmail.com>
2016-06-23 14:26:58 -0500

    comment explaining scipy/scikit-learn install issues

--- 0099FF-stack/web/web.sh
+++ 0099FF-stack/web/web.sh
@@ -5,6 +5,7 @@ sudo pip install -r requirements.txt
 
 #
 # Scipy and Sckit-learn must be installed AFTER numpy, in a separate pip install step
+# (https://github.com/scikit-learn/scikit-learn/issues/4164#issuecomment-100391246)
 # This also means that they cannot be built as wheels, so they are not in the normal requirements.txt file
 #
 sudo pip install -r requirements2.txt
