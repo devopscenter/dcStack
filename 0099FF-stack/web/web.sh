c18b716b1b8b9f5726a145c81702aedc72f3aa73
Bob Lozano <boblozano@gmail.com>
2016-06-23 14:21:46 -0500

    install scipy and scikit-learn in a separate step, after numpy

--- 0099FF-stack/web/web.sh
+++ 0099FF-stack/web/web.sh
@@ -3,6 +3,12 @@
 pushd /installs
 sudo pip install -r requirements.txt
 
+#
+# Scipy and Sckit-learn must be installed AFTER numpy, in a separate pip install step
+# This also means that they cannot be built as wheels, so they are not in the normal requirements.txt file
+#
+sudo pip install -r requirements2.txt
+
 popd
 
 echo "Installed customer-specific web and worker portion"
