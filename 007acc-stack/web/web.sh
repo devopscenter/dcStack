97e3c6e522b793e2a1bc6211acf06f440f94bd41
Gregg Jensen <gjensen@devops.center>
2016-09-29 12:19:34 -0500

    added the necessary lines to the 007acc stack web.sh to get a more recent (v6) LTS version of node.js

--- 007acc-stack/web/web.sh
+++ 007acc-stack/web/web.sh
@@ -2,14 +2,15 @@
 
 sudo pip install -r requirements.txt
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 
 sudo apt-get install -y nodejs
 
-curl -L https://npmjs.com/install.sh | sudo sh
+sudo apt-get install -y build-essential 
+#curl -L https://npmjs.com/install.sh | sudo sh
 
-sudo npm install -g less
+#sudo npm install -g less
 
-sudo npm install -g coffee-script
+#sudo npm install -g coffee-script
 
 echo "Installed customer-specific web and worker portion"
