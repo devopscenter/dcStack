dfdf2ff938a16b0ea3f5759728c1e58dc30edd18
Gregg Jensen <gjensen@devops.center>
2016-09-29 12:22:22 -0500

    remove the commented out section I did with the last commit for the 007acc stack web.sh as they were commented out in error.

--- 007acc-stack/web/web.sh
+++ 007acc-stack/web/web.sh
@@ -7,10 +7,11 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y build-essential 
-#curl -L https://npmjs.com/install.sh | sudo sh
 
-#sudo npm install -g less
+curl -L https://npmjs.com/install.sh | sudo sh
 
-#sudo npm install -g coffee-script
+sudo npm install -g less
+
+sudo npm install -g coffee-script
 
 echo "Installed customer-specific web and worker portion"
