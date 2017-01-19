0cf2fe762b2417e67933a333ca5fa0ae7d7723b6
Gregg Jensen <gjensen@devops.center>
2017-01-19 12:44:25 -0600

    there were quotes in the variables because they are in the ENV.env file that way.  Code has been added here to remove any quoteing around the SYSLOG* variables.  Yes, we could say that the user shouldn't put them in quotes, but that probably won't be remembered by everyone a year or more from now.

--- logging/set-destination.sh
+++ logging/set-destination.sh
@@ -2,23 +2,40 @@
 
 # Called at creation time for instances or containers (e.g. docker compose)
 
+
+stripQuotes ()
+{
+    VAR_ORIG=$1
+    # get rid of any quotes that may be around the string
+    VAR_ORIG_TMP1="${VAR_ORIG%\"}"
+    VAR_ORIG_TMP2="${VAR_ORIG_TMP1#\"}"
+
+    VAR_ORIG_TMP3="${VAR_ORIG_TMP2%\'}"
+    RET_NEW_VAR="${VAR_ORIG_TMP3#\'}"
+
+    echo "${RET_NEW_VAR}"
+}
+
 # put the /etc/environment in the current env for this session...normally would have to log out and log in to get it.
 while IFS='' read -r line || [[ -n "${line}" ]]
 do
-	if [[ "${line}" && "${line}" != "#"* ]]; then
-		export "${line}"
-	fi
+    if [[ "${line}" && "${line}" != "#"* ]]; then
+        export "${line}"
+    fi
 done < /etc/environment
 
+PT_SERVER=$(stripQuotes "${SYSLOG_SERVER}")
+PT_PORT=$(stripQuotes "${SYSLOG_PORT}")
+PT_PROTO=$(stripQuotes "${SYSLOG_PROTO}")
 
-PAPERTRAIL_ADDRESS="${SYSLOG_SERVER}:${SYSLOG_PORT}"
+PAPERTRAIL_ADDRESS="${PT_SERVER}:${PT_PORT}"
 echo "Papertrail destination -> " $PAPERTRAIL_ADDRESS
 
 # Tell rsyslogd where to find papertrail
 # filename starts with "95" so that it is the last config file loaded.
 if [[ ! -f /etc/rsyslog.d/95-papertrail.conf ]]; then
     # check for lower case value
-    if [[ "${SYSLOG_PROTO,,}" == "udp" ]]; then
+    if [[ "${PT_PROTO,,}" == "udp" ]]; then
         echo "*.* @${PAPERTRAIL_ADDRESS}" | sudo tee /etc/rsyslog.d/95-papertrail.conf > /dev/null
     else
         # they have chose tcp or anything else
