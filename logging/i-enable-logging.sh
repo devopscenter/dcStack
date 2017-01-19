08c3f3a03d229a9e709661e97bfed0826cd94c19
Gregg Jensen <gjensen@devops.center>
2017-01-19 11:26:23 -0600

    fix up the logging for an instance to get the destination address from the environment (that comes from deployenv.sh) and also allows the destination to use either upd or tcp based upon the SYSLOG_PROTO

--- logging/i-enable-logging.sh
+++ logging/i-enable-logging.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "i-enable-logging.sh"
-echo "full env $(env)"
-echo "SYSLOG_SERVER=${SYSLOG_SERVER}"
-echo "SYSLOG_PORT=${SYSLOG_PORT}"
-echo "SYSLOG_PROTO=${SYSLOG_PROTO}"
-
-PAPERTRAIL_ADDRESS=$1
-PAPERTRAIL_SERVER=$(echo "$PAPERTRAIL_ADDRESS"|awk -F':' '{print $1}')
-PAPERTRAIL_PORT=$(echo "$PAPERTRAIL_ADDRESS"|awk -F':' '{print $2}')
-
 # note that this is only run on an instance, not within a container.
 # also, must be run after supervisor is installed.
 
@@ -17,5 +7,5 @@ PAPERTRAIL_PORT=$(echo "$PAPERTRAIL_ADDRESS"|awk -F':' '{print $2}')
 ./papertrail.sh
 
 # Set the papertrail destination in rsyslogd and supervisor.
-./set-destination.sh $PAPERTRAIL_ADDRESS
+./set-destination.sh
 
--- logging/set-destination.sh
+++ logging/set-destination.sh
@@ -2,32 +2,23 @@
 
 # Called at creation time for instances or containers (e.g. docker compose)
 
-# override environment variable, if passed via argument
-if [[ -n $1 ]]; then
-    PAPERTRAIL_ADDRESS=$1
-fi
-
+PAPERTRAIL_ADDRESS="${SYSLOG_SERVER}:${SYSLOG_PORT}"
 echo "Papertrail destination -> " $PAPERTRAIL_ADDRESS
-
-PAPERTRAIL_SERVER=$(echo "$PAPERTRAIL_ADDRESS"|awk -F':' '{print $1}')
-PAPERTRAIL_PORT=$(echo "$PAPERTRAIL_ADDRESS"|awk -F':' '{print $2}')
 
 # Tell rsyslogd where to find papertrail
 # filename starts with "95" so that it is the last config file loaded.
 if [[ ! -f /etc/rsyslog.d/95-papertrail.conf ]]; then
-echo "*.* @${PAPERTRAIL_ADDRESS}" | sudo tee /etc/rsyslog.d/95-papertrail.conf > /dev/null
+    # check for lower case value
+    if [[ "${SYSLOG_PROTO,,}" == "udp" ]]; then
+        echo "*.* @${PAPERTRAIL_ADDRESS}" | sudo tee /etc/rsyslog.d/95-papertrail.conf > /dev/null
+    else
+        # they have chose tcp or anything else
+        echo "*.* @@${PAPERTRAIL_ADDRESS}" | sudo tee /etc/rsyslog.d/95-papertrail.conf > /dev/null
+    fi
 fi
 
 # restart rsyslog, in order to utilize new config
 sudo /etc/init.d/rsyslog restart
 
-# replace PAPERTRAIL_SERVER and PAPERTRAIL_PORT with actual values, if they are in the supervisor config file
-# otherwise they'll be passed into the plugin via the environment.
-if [[ -f /etc/supervisor/supervisord.conf ]]; then
-    sudo sed -i "s/PAPERTRAIL_SERVER/${PAPERTRAIL_SERVER}/" /etc/supervisor/supervisord.conf
-    sudo sed -i "s/PAPERTRAIL_PORT/${PAPERTRAIL_PORT}/" /etc/supervisor/supervisord.conf
-fi
-
 # Finally, restart rsyslogd and supervisor
-sudo /etc/init.d/rsyslog restart
 sudo /etc/init.d/supervisor restart
