04aea6334214e75ece03688cd5d82dcf12efaebd
Bob Lozano <boblozano@gmail.com>
2017-01-20 18:27:43 -0600

    update nginx to 1.11.8 and pcre to 8.39

--- web/python-nginx/nginx.sh
+++ web/python-nginx/nginx.sh
@@ -9,9 +9,9 @@ sudo cp conf/sudo-uwsgi /etc/sudoers.d
 sudo apt-fast install -y rsyslog-gnutls
 
 pushd /tmp
-wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2 && \
-    tar -xvf pcre-8.38.tar.bz2
-pushd pcre-8.38 
+wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.bz2 && \
+    tar -xvf pcre-8.39.tar.bz2
+pushd pcre-8.39 
 ./configure && make --silent -j 3 && sudo make --silent install
 popd
 
