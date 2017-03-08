f4a37d443cb9df8e319ebc373e4865a0bbb8192e
Bob Lozano <boblozano@gmail.com>
2017-03-08 15:39:22 -0600

    Ensure that scipy and scikit-learn use the correct numpy

--- 0099FF-stack/web/web.sh
+++ 0099FF-stack/web/web.sh
@@ -6,7 +6,8 @@ sudo pip install -r requirements.txt
 # Scipy and Sckit-learn must be installed AFTER numpy, in a separate pip install step
 # (https://github.com/scikit-learn/scikit-learn/issues/4164#issuecomment-100391246)
 # This also means that they cannot be built as wheels, so they are not in the normal requirements.txt file
+# In order to ensure that wheels are not used, must use the --no-cache-dir option.
 #
-sudo pip install -r requirements2.txt
+sudo pip install --no-cache-dir -r requirements2.txt
 
 echo "Installed customer-specific web and worker portion"
