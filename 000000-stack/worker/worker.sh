057207dead3838797b9d8ba3721c0578884ddb56
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-09 10:21:31 -0600

    Move the ADD's of conf files from the Dockerfiles to the worker.sh and add a worker.sh for the 000000 stack.

---
+++ 000000-stack/worker/worker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+cp supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
+cp supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
--- 66CCFF-stack/worker/worker.sh
+++ 66CCFF-stack/worker/worker.sh
@@ -6,4 +6,7 @@ sudo useradd celery
 sudo mkdir -p /data/deploy /data/media /data/media/pdfcreator /data/media/reports/pdf /data/scratch 
 sudo chown celery:celery /data/media/pdfcreator /data/media/reports /data/media/reports/pdf /data/scratch 
 
+cp conf/supervisor-flower.conf /etc/supervisor/conf.d/flower.conf 
+cp conf/supervisor-celery.conf /etc/supervisor/conf.d/celery.conf
+cp conf/run_celery.sh /etc/supervisor/conf.d/run_celery.sh
 
