5add23326c9a90e221bda57049148366784d2957
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-09 13:20:47 -0600

    remove the mkdir as it is handled by the django-code-deploy execution on jenkins

--- 000000-stack/worker/worker.sh
+++ 000000-stack/worker/worker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 
-sudo mkdir -p /data/deploy /data/media /data/scratch
-
 cp supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
 cp supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
