2933b15bd91b49d77d67b4dd4e905c62fc2d09cc
Gregg Jensen <gjensen@devops.center>
2017-03-10 13:27:44 -0600

    modify the Dockerfile and worker.sh to allow for a app-stack call to a script to possibly overwrite the base level configurations.

--- 000000-stack/worker/worker.sh
+++ 000000-stack/worker/worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-cp supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
-cp supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
+cp app-conf/supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
+cp app-conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
--- 007acc-stack/worker/worker.sh
+++ 007acc-stack/worker/worker.sh
@@ -6,7 +6,7 @@ sudo useradd celery
 sudo mkdir -p /data/deploy /data/media /data/scratch 
 sudo chown celery:celery /data/scratch 
 
-cp conf/supervisor-flower.conf /etc/supervisor/conf.d/flower.conf 
-cp conf/supervisor-celery.conf /etc/supervisor/conf.d/celery.conf
-cp conf/run_celery.sh /etc/supervisor/conf.d/run_celery.sh
+cp app-conf/supervisor-flower.conf /etc/supervisor/conf.d/flower.conf 
+cp app-conf/supervisor-celery.conf /etc/supervisor/conf.d/celery.conf
+cp app-conf/run_celery.sh /etc/supervisor/conf.d/run_celery.sh
 
