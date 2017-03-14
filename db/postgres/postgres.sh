e3b9ae51f8e4a4662dcee1f2fd4ed8979ddd409a
Bob Lozano <boblozano@gmail.com>
2017-03-14 11:09:57 -0500

    update wal-e and s3cmd versions used by postgres

--- db/postgres/postgres.sh
+++ db/postgres/postgres.sh
@@ -44,7 +44,7 @@ sudo locale-gen en_US.UTF-8 && \
 sudo apt-fast -qq -y install libffi-dev
 sudo pip install --upgrade distribute
 sudo pip install -U six && \
-    sudo pip install wal-e==0.8.1
+    sudo pip install wal-e==1.0.2
 sudo apt-fast -qq install -y daemontools lzop pv
 sudo pip install -U requests
 
@@ -65,5 +65,5 @@ sudo service postgresql stop
 #disable init.d autostart
 sudo update-rc.d postgresql disable
 
-sudo pip install s3cmd==1.6.0
+sudo pip install s3cmd==1.6.1
 sudo pip install -U setuptools
