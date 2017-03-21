5e0e6362b41b612eb4fc1b95dcefb289fe913daf
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-21 14:07:55 -0500

    add a devops.center enviroment file that can be sourced into other shell scripts that will provide shell functions to make writting console messages a bit more meaningful.  This is only useful for shell scripts and not python scripts.

---
+++ dcEnv.sh
@@ -0,0 +1,49 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: dcEnv.sh
+# 
+#         USAGE: ./dcEnv.sh 
+# 
+#   DESCRIPTION: script to include in other scripts that set up the environment
+#                and provides some basic utility functions
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 03/21/2017 12:02:46
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+dcLog()
+{
+    msg=$1
+    state=$2
+
+    scriptName=$(basename -- "$0")
+    TIMESTAMP=$(date +%F_%T)
+
+    if [[ ! -z ${state} ]]; then
+        echo "[${TIMESTAMP}]:${scriptName}:${state}:${msg}"
+    else
+        echo "[${TIMESTAMP}]:${scriptName}::${msg}"
+    fi
+}
+
+dcStartLog()
+{
+    msg=$1
+    dcLog ${msg} "START"
+}
+
+dcEndLog()
+{
+    msg=$1
+    dcLog ${msg} "END"
+}
+
