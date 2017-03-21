0385fe6bbb594ce891cdd90c1e4ba1ae08d3418e
Bob Lozano <boblozano@gmail.com>
2017-03-21 16:32:58 -0500

    66CCFF: fix config distributions, add start/stop logs

--- 66CCFF-stack/web/web.sh
+++ 66CCFF-stack/web/web.sh
@@ -1,23 +1,41 @@
-#!/bin/bash -e
-
+#!/bin/bash - 
+#===============================================================================
 #
-# App-specific web install for 66CCFF
-#
+#          FILE: web.sh
+# 
+#         USAGE: web/web.sh
+# 
+#   DESCRIPTION: 66CCFF stack, install web-specific components.
+# 
+#       OPTIONS: ===
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Bob Lozano, bob@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: long time ago
+#      REVISION:  ---
+#===============================================================================
 
-echo "Begin: install of customer-specific web portion"
+#set -o nounset                             # Treat unset variables as an error
 
+source ../../dcENV.sh                       # initalize logging environment
+
+dcStartLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
+
+# Some equired packages for rendering
 sudo apt-fast -y install libfontconfig-dev libxrender-dev libxtst6
 
+# install node
 curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
-
 sudo apt-get install -y nodejs
-
 curl -L https://npmjs.com/install.sh | sudo sh
-
 sudo npm install -g less
 
+# Install required packages
 sudo pip install -r requirements.txt
 
+# prepare required directories for this app
 sudo mkdir -p /data/deploy /data/media /data/media/pdfcreator /data/media/reports/pdf /data/scratch 
 
-echo "End: install of customer-specific web portion"
+dcEndLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
--- 66CCFF-stack/worker/worker.sh
+++ 66CCFF-stack/worker/worker.sh
@@ -1,13 +1,29 @@
-#!/bin/bash -e
-set -x
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: worker.sh
+# 
+#         USAGE: worker/worker.sh  COMBINED_WEBANDWORKER
+# 
+#   DESCRIPTION: 66CCFF stack, install worker-specific components.
+# 
+#       OPTIONS: if COMBINED_WEBANDWORKER="true", then web and worker on a single instance/container; otherwise, separate instances/containers.
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Bob Lozano, bob@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: long time ago
+#      REVISION:  ---
+#===============================================================================
 
-#
-# One time installation tasks for a 66CCFF worker.
-# Assumes that ../web/web.sh has been run.
-#
+#set -o nounset                             # Treat unset variables as an error
+
+source ../../dcENV.sh                       # initalize logging environment
+
 COMBINED_WEBANDWORKER=$1
 
-echo "Begin: install of customer-specific worker, combo: ",COMBINED_WEBANDWORKER
+dcStartLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
 
 sudo useradd celery
 
@@ -15,7 +31,7 @@ sudo useradd celery
 # If this is purely a worker, then we don't need uwsgi (this app still requires nginx, though with a specialized config)
 #
 
-if [[ ! $COMBINED_WEBANDWORKER ]]; then
+if [[ ! "${COMBINED_WEBANDWORKER}" = "true" ]]; then
     sudo rm -rf /etc/supervisor/conf.d/uwsgi.conf
     sudo rm -rf /etc/supervisor/conf.d/run_uwsgi.conf
     sudo cp conf/nginx.conf /usr/local/nginx/conf/nginx.conf
@@ -27,8 +43,13 @@ fi
 sudo mkdir -p /data/deploy /data/media /data/media/pdfcreator /data/media/reports/pdf /data/scratch 
 sudo chown celery:celery /data/media/pdfcreator /data/media/reports /data/media/reports/pdf /data/scratch 
 
+#
+# Setup supervisor to run flower and celery
+#
 sudo cp conf/supervisor-flower.conf /etc/supervisor/conf.d/flower.conf 
 sudo cp conf/supervisor-celery.conf /etc/supervisor/conf.d/celery.conf
 sudo cp conf/run_celery.sh /etc/supervisor/conf.d/run_celery.sh
 
-echo "End: install of customer-specific worker, combo: ", COMBINED_WEBANDWORKER
+
+dcEndLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
+
