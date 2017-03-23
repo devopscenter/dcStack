271dd2a449ece6833fc44a2463454d2688bc6aba
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-23 08:54:02 -0500

    add tmux installation to the base set of code to install.

--- buildtools/utils/base-utils.sh
+++ buildtools/utils/base-utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ev
 sudo apt-get -qq update && sudo apt-get -qq -y install python-software-properties software-properties-common && \
     sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
+    sudo add-apt-repository -yu ppa:pi-rho/dev \
     sudo apt-get -qq update
 
 sudo add-apt-repository -y ppa:saiarcot895/myppa && \
@@ -11,9 +12,9 @@ sudo apt-fast -qq -y install git wget sudo vim unzip curl language-pack-en
 
 sudo apt-fast -y install ncdu ntp fail2ban htop
 
-#sudo add-apt-repository -y ppa:tualatrix/ppa
-#sudo apt-fast update
-#sudo apt-fast -y install ubuntu-tweak
+sudo apt-get install tmux-next
+#sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu16.04.1
+sudo mv /usr/bin/tmux-next /usr/bin/tmux
 
 pushd /tmp
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
