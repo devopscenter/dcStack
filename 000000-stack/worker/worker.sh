c4fb6bd13cdcb9925bcbe99607b19b93f9bcb41a
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-23 14:59:57 -0500

    change the name of the conf directory to read from app-conf to conf.

--- 000000-stack/worker/worker.sh
+++ 000000-stack/worker/worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-cp app-conf/supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
-cp app-conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
+cp conf/supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
+cp conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
