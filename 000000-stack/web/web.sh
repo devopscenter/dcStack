8d7362b535785b5c79d4597c3e63903f790fa6bc
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-23 14:27:30 -0500

    correct the 000000-stack web.sh

--- 000000-stack/web/web.sh
+++ 000000-stack/web/web.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
-CUST_APP_NAME=$1
-ENV=$2
-
-#-------------------------------------------------------------------------------
-# run the appliction specific web_commands.sh 
-#-------------------------------------------------------------------------------
-if [[ -e "${HOME}/${CUST_APP_NAME}/${CUST_APP_NAME}-utils/config/${ENV}/web-commands.sh" ]]; then
-    sudo "${HOME}/${CUST_APP_NAME}/${CUST_APP_NAME}-utils/config/${ENV}/web-commands.sh"
-fi
 
 cp requirements.txt /installs/requirements.txt
 cp requirements2.txt /installs/requirements2.txt
@@ -19,7 +10,8 @@ sudo pip install -r requirements.txt
 # Scipy and Sckit-learn must be installed AFTER numpy, in a separate pip install step
 # (https://github.com/scikit-learn/scikit-learn/issues/4164#issuecomment-100391246)
 # This also means that they cannot be built as wheels, so they are not in the normal requirements.txt file
+# In order to ensure that wheels are not used, must use the --no-cache-dir option.
 #
-sudo pip install -r requirements2.txt
+sudo pip install --no-cache-dir -r requirements2.txt
 
 echo "Installed customer-specific web and worker portion"
