df1956a93dbad7afdbd7e5f603a7473025b94609
Bob Lozano <boblozano@gmail.com>
2017-03-22 20:03:48 -0500

    clean up logging messages for 66CCFF

--- 66CCFF-stack/web/web.sh
+++ 66CCFF-stack/web/web.sh
@@ -21,7 +21,7 @@
 
 source ../../dcEnv.sh                       # initalize logging environment
 
-dcStartLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
+dcStartLog "install of customer-specific web"
 
 # Some equired packages for rendering
 sudo apt-fast -y install libfontconfig-dev libxrender-dev libxtst6
@@ -38,4 +38,4 @@ sudo pip install -r requirements.txt
 # prepare required directories for this app
 sudo mkdir -p /data/deploy /data/media /data/media/pdfcreator /data/media/reports/pdf /data/scratch 
 
-dcEndLog "install of customer-specific worker, combo: ${COMBINED_WEBANDWORKER}"
+dcEndLog "install of customer-specific web"
