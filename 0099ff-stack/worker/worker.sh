ebe72d4f039e060084ed82981fde98109aa5c731
Bob Lozano <boblozano@gmail.com>
2017-04-12 16:21:49 -0500

    update 99ccff to solve numpy, pandas, scipy, scikit-learn dependency problems; also update logging on install

--- 0099ff-stack/worker/worker.sh
+++ 0099ff-stack/worker/worker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
+source ../../dcEnv.sh                       # initalize logging environment
+
+dcStartLog "install of app-specific worker for 0099ff"
+
 cp app-conf/supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
 cp app-conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
+
+dcEndLog "install of app-specific worker for 0099ff"
