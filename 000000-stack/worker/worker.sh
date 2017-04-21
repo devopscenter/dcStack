2698e31cb54092930f44db9cb0c3e7778bea46e0
Bob Lozano <boblozano@gmail.com>
2017-04-21 10:17:49 -0500

    tweaks to app stack 000000 for new config, logging structures

--- 000000-stack/worker/worker.sh
+++ 000000-stack/worker/worker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
+COMBINED_WEB_WORKER="${1}"
+
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
-dcStartLog "install of app-specific worker for 000000"
+dcStartLog "install of app-specific worker for 000000, combo: ${COMBINED_WEB_WORKER}"
 
 #
-# If this is purely a worker, then we don't need uwsgi (this app still requires nginx, though with a specialized config)
+# If this is purely a worker, then we don't need nginx and uwsgi
 #
 
-if [[ "${COMBINED_WEB_WORKER}" = "true" ]]; then
-    sudo cp conf/nginx-combo.conf /usr/local/nginx/conf/nginx.conf
-elif [[ "${COMBINED_WEB_WORKER}" = "false" ]]; then
+if [[ "${COMBINED_WEB_WORKER}" = "false" ]]; then
     sudo rm -rf /etc/supervisor/conf.d/uwsgi.conf
     sudo rm -rf /etc/supervisor/conf.d/run_uwsgi.conf
     sudo rm -rf /etc/supervisor/conf.d/nginx.conf 
@@ -21,4 +21,4 @@ fi
 cp conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
 
 
-dcEndLog "install of app-specific worker for 000000"
+dcEndLog "install of app-specific worker for 000000, combo: ${COMBINED_WEB_WORKER}"
