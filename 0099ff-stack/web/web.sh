c54cf147cad800a51708f6fde561b2d73093b305
Bob Lozano <boblozano@gmail.com>
2017-04-21 10:43:10 -0500

    new config, logging structures for app stack 0099ff

--- 0099ff-stack/web/web.sh
+++ 0099ff-stack/web/web.sh
@@ -3,10 +3,6 @@
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
 dcStartLog "install of app-specific web for 0099ff"
 
-cp requirements.txt /installs/requirements.txt
-cp requirements2.txt /installs/requirements2.txt
-cp web.sh /installs/web.sh
-
 sudo pip install -r requirements.txt
 
 dcEndLog "install of app-specific web for 0099ff"
