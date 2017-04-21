553a6e5354dbe48b13d07f810a7e589684a98fd0
Bob Lozano <boblozano@gmail.com>
2017-04-21 10:50:07 -0500

    new config, logging structures for app stack 9900af

--- 9900af-stack/web/web.sh
+++ 9900af-stack/web/web.sh
@@ -3,9 +3,6 @@
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
 dcStartLog "install of app-specific web for 9900af"
 
-cp requirements.txt /installs/requirements.txt
-cp web.sh /installs/web.sh
-
 sudo pip install -r requirements.txt
 
 dcEndLog "install of app-specific web for 9900af"
--- 9900af-stack/worker/worker.sh
+++ 9900af-stack/worker/worker.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -e
 
+COMBINED_WEB_WORKER="${1}"
+
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
-dcStartLog "install of app-specific worker for 9900af"
+dcStartLog "install of app-specific worker for 9900af, combo: ${COMBINED_WEB_WORKER}"
 
-cp app-conf/supervisor-djangorq-worker.conf /installs/supervisor-djangorq-worker.conf
-cp app-conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
+#
+# If this is purely a worker, then we don't need nginx and uwsgi
+#
 
-dcEndLog "install of app-specific worker for 9900af"
+if [[ "${COMBINED_WEB_WORKER}" = "false" ]]; then
+    sudo rm -rf /etc/supervisor/conf.d/uwsgi.conf
+    sudo rm -rf /etc/supervisor/conf.d/run_uwsgi.conf
+    sudo rm -rf /etc/supervisor/conf.d/nginx.conf 
+fi
+
+#
+# Setup supervisor to run django-rq worker(s)
+#
+cp conf/supervisor-djangorq-worker.conf /etc/supervisor/conf.d/djangorq-worker.conf
+
+dcEndLog "install of app-specific worker for 9900af, combo: ${COMBINED_WEB_WORKER}"
