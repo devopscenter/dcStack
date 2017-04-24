7023250551dd1b268ad60bb9c02143cfb645167c
Gregg Jensen <gjensen@devops.center>
2017-04-24 17:31:53 -0500

    make the script adhere to the new devops.center standard structure and logging output with more comments.

--- db/postgres-standby/init-standby.sh
+++ db/postgres-standby/init-standby.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+#===============================================================================
 #
-# Docker Stack - Docker stack to manage infrastructures
+#          FILE: init-standby.sh
+# 
+#         USAGE: ./init-standby.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 04/24/2017 16:50:58
+#      REVISION:  ---
 #
 # Copyright 2014 devops.center
 #
@@ -15,21 +29,57 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-set -ex
+#===============================================================================
+
+#set -o nounset         # Treat unset variables as an error
+#set -x                  # essentially debug mode
+set -o errexit          # exit immediately if command exists with a non-zero status
+
+#-------------------------------------------------------------------------------
+# grab the functions to pretty up the logging
+#-------------------------------------------------------------------------------
+source  /usr/local/bin/dcEnv.sh
+
+dcStartLog "initialize database follower/standby"
+
+#-------------------------------------------------------------------------------
+dcLog "First stop postgres"
+#-------------------------------------------------------------------------------
 sudo supervisorctl stop postgres
+
+
+#-------------------------------------------------------------------------------
+dcLog "now remove the xlog files"
+#-------------------------------------------------------------------------------
 #sudo unlink /media/data/postgres/db/pgdata/pg_xlog
 sudo rm -rf /media/data/postgres/db/pgdata/pg_xlog
 sudo rm -rf /media/data/postgres/db/pgdata/ /media/data/postgres/db/pg_xlog/ /media/data/postgres/xlog/*
+
 #ln -s /media/data/postgres/xlog /media/data/postgres/db/pg_xlog
 #sudo chown -R postgres:postgres /media/data/postgres/xlog
+
+#-------------------------------------------------------------------------------
+dcLog "start the base backup on pgmaster-1 and set up a stream back to this machine"
+#-------------------------------------------------------------------------------
 sudo pg_basebackup -D /media/data/postgres/db/pgdata -w -R --xlog-method=stream --dbname="host=pgmaster-1 user=postgres"
+
+
+#-------------------------------------------------------------------------------
+dcLog "while the backup is streaming from pgmaster-1, copy the xlog entries back to this machine"
+#-------------------------------------------------------------------------------
 sudo rsync -av /media/data/postgres/db/pgdata/pg_xlog/ /media/data/postgres/xlog
 sudo rm -rf /media/data/postgres/db/pgdata/pg_xlog
 sudo ln -s /media/data/postgres/xlog/ /media/data/postgres/db/pgdata/pg_xlog
 sudo chown -R postgres:postgres /media/data/postgres/db /media/data/postgres/xlog
 
-# replace master's conf file so wal-e backups go to the correct bucket if promoted
+#-------------------------------------------------------------------------------
+dcLog "replace master's conf file so wal-e backups go to the correct bucket if promoted"
+#-------------------------------------------------------------------------------
 sudo cp --preserve /media/data/postgres/backup/postgresql.conf /media/data/postgres/db/pgdata/
 
+
+#-------------------------------------------------------------------------------
+dcLog "and finally start postgres"
+#-------------------------------------------------------------------------------
 sudo supervisorctl start postgres
+dcEndLog "Finished..."
