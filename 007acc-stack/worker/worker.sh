4138bad887092b8aa5a30fa3f4faabfeb22d2280
Bob Lozano <boblozano@gmail.com>
2017-04-28 14:20:15 -0500

    remove superfilous temp directories for 007acc

--- 007acc-stack/worker/worker.sh
+++ 007acc-stack/worker/worker.sh
@@ -21,20 +21,6 @@ if [[ "${COMBINED_WEB_WORKER}" = "false" ]]; then
     sudo rm -rf /etc/supervisor/conf.d/nginx.conf
 fi
 
-# 
-# Required directories for this app
-#
-if [[ "${SCRATCHVOLUME}" == "true" ]]; then
-    sudo mkdir /media/data/scratch
-
-    sudo ln -s /media/data /data/media 
-    sudo ln -s /media/data/scratch /data/scratch
-else
-    # put everything on the root volume
-    sudo mkdir -p /data/media/pdfcreator /data/media/reports/pdf /data/scratch 
-fi
-sudo chown celery:celery /data/scratch 
-
 #
 # Setup supervisor to run flower and celery
 #
