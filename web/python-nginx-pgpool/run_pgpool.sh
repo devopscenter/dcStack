d8f1bb514b5b03358bf6cc4ac3436a01285595dc
Bob Lozano <boblozano@gmail.com>
2017-05-23 23:37:03 -0500

    make sure that pgpool checks backend status on startup

--- web/python-nginx-pgpool/run_pgpool.sh
+++ web/python-nginx-pgpool/run_pgpool.sh
@@ -10,5 +10,9 @@ fi
 if [ ! -d /var/run/postgresql ]; then
   sudo install -d -m 755 -o postgres -g postgres /var/run/postgresql
 fi
+# Make sure that pgpool does not read in any pre-existing status file, so that it will query backends on startup
+# to see whether they are reachable or not. This is the preferred beahvior, UNLESS the pgpool config permits pgpool
+# to initiate postgres follower promotions.
 
-exec /usr/local/bin/pgpool -a /etc/pgpool2/pool_hba.conf -f $1 -F /etc/pgpool2/pcp.conf -n
+exec /usr/local/bin/pgpool -a /etc/pgpool2/pool_hba.conf -f $1 -F /etc/pgpool2/pcp.conf -n -D
+
