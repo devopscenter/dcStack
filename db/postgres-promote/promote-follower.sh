0074b45301cbdb947f027d145613f4145e297417
Gregg Jensen <gjensen@devops.center>
2017-06-08 14:59:07 -0500

    Begin the clean in prepartion for route53 changes

--- db/postgres-promote/promote-follower.sh
+++ db/postgres-promote/promote-follower.sh
@@ -1,37 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: promote-follower.sh
+# 
+#         USAGE: ./promote-follower.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 06/08/2017 14:52:58
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+#set -x 
 
 OLD_MASTER_PRIVATEIP=$1
 OLD_FOLLOWER_PRIVATEIP=$2
 
 # remove old follower ip/hostname
-function etc_hosts_remove
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  etc_hosts_remove
+#   DESCRIPTION:  function to clean up etc/hosts if we end up using it over route53
+#    PARAMETERS:  
+#       RETURNS:  
+#-------------------------------------------------------------------------------
+etc_hosts_remove()
 {
-OLD_HOSTS_IP=$1
-OLD_HOSTS_NAME=$2
-TMP_ETC_HOSTS=$(mktemp -t etchosts.XXXXXX)
-while read -r i; do
-  # make sure it's not a comment
-  if (echo "$i"|grep -vq "^#"); then
-    # if it's the only hostname entry for an IP, comment out the line
-    if (echo "$i"|grep -Eq "^${OLD_HOSTS_IP}\b[[:blank:]]*\b${OLD_HOSTS_NAME}\b[[:space:]]*$"); then
-      echo "$i" | sed "s/^${OLD_HOSTS_IP}[[:blank:]]*\b${OLD_HOSTS_NAME}\b[[:space:]]*$/#&/" | sudo tee -a "$TMP_ETC_HOSTS"
-      continue
-    # if other hostnames are associated with an IP, remove $OLD_HOSTS_NAME and any whitespace before it
-    elif (echo "$i"|grep -Eq "^${OLD_HOSTS_IP}\b[[:blank:]]*.*\b${OLD_HOSTS_NAME}\b[[:blank:]]*"); then
-      echo "$i" | sed "s/[[:blank:]]*\b${OLD_HOSTS_NAME}\b//g" | sudo tee -a "$TMP_ETC_HOSTS"
-      continue
+    OLD_HOSTS_IP=$1
+    OLD_HOSTS_NAME=$2
+    TMP_ETC_HOSTS=$(mktemp -t etchosts.XXXXXX)
+    while read -r i; do
+    # make sure it's not a comment
+    if (echo "$i"|grep -vq "^#"); then
+        # if it's the only hostname entry for an IP, comment out the line
+        if (echo "$i"|grep -Eq "^${OLD_HOSTS_IP}\b[[:blank:]]*\b${OLD_HOSTS_NAME}\b[[:space:]]*$"); then
+        echo "$i" | sed "s/^${OLD_HOSTS_IP}[[:blank:]]*\b${OLD_HOSTS_NAME}\b[[:space:]]*$/#&/" | sudo tee -a "$TMP_ETC_HOSTS"
+        continue
+        # if other hostnames are associated with an IP, remove $OLD_HOSTS_NAME and any whitespace before it
+        elif (echo "$i"|grep -Eq "^${OLD_HOSTS_IP}\b[[:blank:]]*.*\b${OLD_HOSTS_NAME}\b[[:blank:]]*"); then
+        echo "$i" | sed "s/[[:blank:]]*\b${OLD_HOSTS_NAME}\b//g" | sudo tee -a "$TMP_ETC_HOSTS"
+        continue
+        fi
     fi
-  fi
-  echo "$i" | sudo tee -a "$TMP_ETC_HOSTS" > /dev/null
-done < /etc/hosts
-cat "$TMP_ETC_HOSTS" | sudo tee /etc/hosts > /dev/null
-rm -f "$TMP_ETC_HOSTS"
+    echo "$i" | sudo tee -a "$TMP_ETC_HOSTS" > /dev/null
+    done < /etc/hosts
+    cat "$TMP_ETC_HOSTS" | sudo tee /etc/hosts > /dev/null
+    rm -f "$TMP_ETC_HOSTS"
 }
 
 #etc_hosts_remove "$OLD_MASTER_PRIVATEIP" pgmaster-1
 #etc_hosts_remove "$OLD_FOLLOWER_PRIVATEIP" pgstandby-1
 
 #echo "${OLD_FOLLOWER_PRIVATEIP} postgressmaster_1" | sudo tee -a /etc/hosts > /dev/null
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  cleanup_route53
+#   DESCRIPTION:  manage the changes in route53 for the switch
+#    PARAMETERS:  
+#       RETURNS:  
+#-------------------------------------------------------------------------------
+cleanup_route53()
+{
+}
+
 
 # update wal-e archive command to point to correct/current hostname
 sudo sed -E -i "s/(^archive_command\b.*s3:\/\/.*\/)([a-zA-Z]+-[a-zA-Z0-9]+)([[:blank:]]+)/\1${HOSTNAME}\3/g" /media/data/postgres/db/pgdata/postgresql.conf
