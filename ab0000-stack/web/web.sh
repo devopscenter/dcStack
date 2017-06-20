4694428200ae6fe0c274758ab91d3b1cd718405e
Bob Lozano <boblozano@gmail.com>
2017-06-20 18:42:53 -0500

    fix invocation of jenkins install

--- ab0000-stack/web/web.sh
+++ ab0000-stack/web/web.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -e
+!/bin/bash -e
+
+set -x
 
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
 dcStartLog "install of app-specific web for ab0000 (basic Jenkins)"
+
+pushd /../../buildtools/jenkins/
+./jenkins-install.sh
+popd
 
 sudo pip install -r requirements.txt
 
