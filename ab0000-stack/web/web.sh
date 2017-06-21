1f0e6899db959a7c129feba6d243f671f69433b1
Bob Lozano <boblozano@gmail.com>
2017-06-20 21:03:38 -0500

    fix up path refs, etc

--- ab0000-stack/web/web.sh
+++ ab0000-stack/web/web.sh
@@ -1,11 +1,11 @@
-!/bin/bash -e
+#!/bin/bash -e
 
-set -x
 
 source /usr/local/bin/dcEnv.sh                       # initalize logging environment
+
 dcStartLog "install of app-specific web for ab0000 (basic Jenkins)"
 
-pushd /../../buildtools/jenkins/
+pushd ../../buildtools/jenkins/
 ./jenkins-install.sh
 popd
 
