211386f36ed4c01059ad037f1e4f366675834e4d
Bob Lozano <boblozano@gmail.com>
2017-07-08 17:46:36 -0500

    bump pcre to 8.41, nginx 1.13.1, uwsgi 2.15

--- web/python-nginx/nginx.sh
+++ web/python-nginx/nginx.sh
@@ -9,8 +9,8 @@ sudo cp conf/sudo-uwsgi /etc/sudoers.d
 sudo apt-fast install -y rsyslog-gnutls
 
 pushd /tmp
-wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.bz2 && \
-    tar -xvf pcre-8.39.tar.bz2
+wget --quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.tar.bz2 && \
+    tar -xvf pcre-8.41.tar.bz2
 pushd pcre-8.39 
 ./configure && make --silent -j 3 && sudo make --silent install
 popd
