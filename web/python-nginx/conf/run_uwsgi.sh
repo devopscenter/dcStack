044fe4477c902a56ee52b018c09f548cca8bf9c8
Bob Lozano <boblozano@gmail.com>
2017-09-05 16:49:58 -0500

    hotfix: permit setting of UWSGI buffer-size by ENV, & set default values for UWSGI config vars settable via ENV

--- web/python-nginx/conf/run_uwsgi.sh
+++ web/python-nginx/conf/run_uwsgi.sh
@@ -12,11 +12,18 @@ if [ -e /tmp/uwsgififo ]; then
     sudo rm /tmp/uwsgififo
 fi
 
+# Set default values for UWSGI configs
+
+UWSGI_WORKERS=${UWSGI_WORKERS:-8}
+UWSGI_MAX_REQUESTS=${UWSGI_MAX_REQUESTS:-5000}
+UWSGI_BUFFER_SIZE=${UWSGI_BUFFER_SIZE:-4096}
+
 exec /usr/local/opt/python/bin/uwsgi \
         --enable-threads \
         --master-fifo /tmp/uwsgififo \
         --workers=${UWSGI_WORKERS} \
         --max-requests=${UWSGI_MAX_REQUESTS} \
+        --buffer-size=${UWSGI_BUFFER_SIZE} \
         --lazy-apps \
         --hook-post-fork="chdir:/data/deploy/current" \
         /data/deploy/current/uwsgi.ini
