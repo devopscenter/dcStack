7611559be85bfc10bc9ed95d4ad5b16432118eff
Bob Lozano <boblozano@gmail.com>
2017-09-13 16:34:42 -0500

    make sure postgres install uses durable ENVs when on an instance

--- db/postgres/supervisorconfig.sh
+++ db/postgres/supervisorconfig.sh
@@ -3,8 +3,10 @@
 PGVERSION=$1
 . ./postgresenv.sh $PGVERSION
 
-#SUPERVISORD
+# Setup supervisor to run postgres
 sudo cp ./conf/supervisor-postgres.conf /etc/supervisor/conf.d/postgres.conf
 sudo cp ./conf/run_postgres.sh /etc/supervisor/conf.d/run_postgres.sh
 echo "export POSTGRES_VERSION=${POSTGRES_VERSION}"  | sudo tee -a /etc/default/supervisor
 
+# If on an instance, drop a .env file so that this will always be set when ENVs are updated at any point in the future.
+if [[ -d "$HOME/.dcConfig/" ]] ; then echo "POSTGRES_VERSION=${POSTGRES_VERSION}" >> "$HOME/.dcConfig/instance-postgres.env"
