8e6fa61ffd4052df5f0f3061ef7e7648ee9d6cac
Gregg Jensen <gjensen@devops.center>
2017-11-29 14:44:23 -0600

    Change the output to crontab to a heredoc

--- buildtools/jenkins/jenkins-install.sh
+++ buildtools/jenkins/jenkins-install.sh
@@ -67,9 +67,12 @@ sudo cp -a conf/run_jenkins.sh /etc/supervisor/conf.d/run_jenkins.sh
 #-------------------------------------------------------------------------------
 theHostName=$(hostname)
 if ! (crontab -l |grep '^[^#].*jenkins-backup.sh\b.*'); then
-    echo "MAILTO=\"\"" | crontab -
-    echo "PATH=/usr/local/opt/python/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" | crontab -
-    echo "11 03  *   *   *     /home/ubuntu/dcStack/buildtools/jenkins/jenkins-backup.sh ${theHostName}" | crontab -
+    cat <<- EOF | crontab -
+    MAILTO=""
+    PATH=/usr/local/opt/python/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+    11 03  *   *   *     /home/ubuntu/dcStack/buildtools/jenkins/jenkins-backup.sh ${theHostName}
+EOF
 fi
 
 # install grunt-cli
