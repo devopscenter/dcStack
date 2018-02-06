ba2153917105aabfeba6ad5be5b0e27682827a6b
Gregg Jensen <gjensen@devops.center>
2018-02-06 17:07:38 -0600

    add a workaround for the supervisor restart using the force-stop option.

--- db/postgres/i-new_postgres.sh
+++ db/postgres/i-new_postgres.sh
@@ -157,6 +157,10 @@ ln -s $HOME/dcUtils/db/restore.sh /media/data/db_restore/
 #-------------------------------------------------------------------------------
 # restart supervisor to pick up new postgres files in conf.d
 #-------------------------------------------------------------------------------
+# NOTE: this is a work around to ensure supervisor is stopped before restarting
+# this is from this page:
+# https://stackoverflow.com/questions/32738415/supervisor-fails-to-restart-half-of-the-time/33881057#33881057
+sudo /etc/init.d/supervisor force-stop && \
 sudo /etc/init.d/supervisor restart
 
 #-------------------------------------------------------------------------------
