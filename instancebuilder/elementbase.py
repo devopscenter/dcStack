ba00e4327ef7f0f6da8869caa56a599d0c5afb82
Gregg Jensen <gjensen@devops.center>
2018-04-18 11:28:04 -0500

    add a more description print statement when running an external script.

--- instancebuilder/elementbase.py
+++ instancebuilder/elementbase.py
@@ -72,6 +72,7 @@ class ElementBase(object):
     def runScript(self, shellScript):
         """Execute the passed in shell script."""
         try:
+            print(self.__class__.__name__ + " EXECUTING: " + shellScript)
             subprocess.call(shellScript, shell=True)
         except subprocess.CalledProcessError:
             print("ERROR: there was a problem running the script: "
