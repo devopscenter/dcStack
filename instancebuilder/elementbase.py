6af8c35006a6f83b2ce83a092bd623835e205cc7
Gregg Jensen <gjensen@devops.center>
2018-04-18 17:08:01 -0500

    remove the printing of the ouput, that version didn't work.  Need to figure it out before trying again.

--- instancebuilder/elementbase.py
+++ instancebuilder/elementbase.py
@@ -75,10 +75,11 @@ class ElementBase(object):
         """Execute the passed in shell script."""
         try:
             print(self.__class__.__name__ + " EXECUTING: " + shellScript)
-            appOutput = subprocess.check_output(shellScript,
-                                                stderr=subprocess.STDOUT,
-                                                shell=True)
-            print(appOutput)
+            subprocess.call(shellScript, shell=True)
+#            appOutput = subprocess.check_output(shellScript,
+#                                                stderr=subprocess.STDOUT,
+#                                                shell=True)
+#            print(appOutput)
         except subprocess.CalledProcessError:
             print("ERROR: there was a problem running the script: "
                   "{}".format(shellScript))
