3b34d0f4fe92ab821ef373d3e04ff1808694147d
Gregg Jensen <gjensen@devops.center>
2018-09-12 16:11:49 -0500

    fix typo looking for the create scratch volume setting.

--- instancebuilder/instanceBuilder.py
+++ instancebuilder/instanceBuilder.py
@@ -106,7 +106,7 @@ class InstanceBuilder:
         # well.
         # ----------------------------------------------------------------------
         deployDir = "/data/deploy"
-        if "SCRATCHVOLUME" in self.elementsToInclude:
+        if "CREATE_SCRATCH_VOLUME" in self.elementsToInclude:
             # hold the original directory
             originalDir = os.getcwd()
             # and change the dcStack postgres directory so that we can run the
