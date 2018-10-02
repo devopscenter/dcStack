fa35c961bf526ddf9b474edfdb24615362e4af40
Gregg Jensen <gjensen@devops.center>
2018-10-02 13:24:57 -0500

    disable the php service from starting automatically by systemctl

--- php/php.sh
+++ php/php.sh
@@ -116,6 +116,12 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 #php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
+# stop the systemd service so that supervisor will control php
+sudo systemctl stop php${PHP_VERSION}-fpm
+# NOTE TODO possible use the mask instead of disable so that it can not be run
+# manually or automatically.  It can be unmasked if needed though
+sudo systemctl disable php${PHP_VERSION}-fpm
+
 dcLog "done"
 
 
