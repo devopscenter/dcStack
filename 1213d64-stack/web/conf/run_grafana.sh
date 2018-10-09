940706f99bbd3487a1062773f17caef176d0c7d8
Gregg Jensen <gjensen@devops.center>
2018-10-09 16:44:13 -0500

    change this so that it runs in the foreground

--- 1213d64-stack/web/conf/run_grafana.sh
+++ 1213d64-stack/web/conf/run_grafana.sh
@@ -55,54 +55,12 @@ MAX_OPEN_FILES=10000
 PID_FILE=/var/run/$NAME.pid
 DAEMON=/usr/sbin/$NAME
 
-
-# overwrite settings from default file
-if [ -f "$DEFAULT" ]; then
-        . "$DEFAULT"
+if [ -n "$MAX_OPEN_FILES" ]; then
+	ulimit -n $MAX_OPEN_FILES
 fi
-
-DAEMON_OPTS="--pidfile=${PID_FILE} --config=${CONF_FILE} cfg:default.paths.data=${DATA_DIR} cfg:default.paths.logs=${LOG_DIR}"
-
-
-#pid=`pidofproc -p $PID_FILE grafana`
-#if [ -n "$pid" ] ; then
-#		log_begin_msg "Already running."
-#		log_end_msg 0
-#		exit 0
-#fi
 
 # Prepare environment
 mkdir -p "$LOG_DIR" "$DATA_DIR" && chown "$GRAFANA_USER":"$GRAFANA_GROUP" "$LOG_DIR" "$DATA_DIR"
 touch "$PID_FILE" && chown "$GRAFANA_USER":"$GRAFANA_GROUP" "$PID_FILE"
 
-if [ -n "$MAX_OPEN_FILES" ]; then
-			ulimit -n $MAX_OPEN_FILES
-	fi
-
-	# Start Daemon
-	start-stop-daemon --start -b --chdir "$WORK_DIR" --user "$GRAFANA_USER" -c "$GRAFANA_USER" --pidfile "$PID_FILE" --exec $DAEMON -- $DAEMON_OPTS
-	return=$?
-	if [ $return -eq 0 ]
-	then
-	  sleep 1
-
-# check if pid file has been written two
-	  if ! [[ -s $PID_FILE ]]; then
-		#log_end_msg 1
-		exit 1
-	  fi
-
-			i=0
-			timeout=10
-			# Wait for the process to be properly started before exiting
-			until { cat "$PID_FILE" | xargs kill -0; } >/dev/null 2>&1
-			do
-					sleep 1
-					i=$(($i + 1))
-  if [ $i -gt $timeout ]; then
-					  #log_end_msg 1
-					  exit 1
-					fi
-			done
-fi
-
+/usr/sbin/grafana-server --pidfile=${PID_FILE} --config=${CONF_DIR} --homepath=${GRAFANA_HOME} cfg:default.paths.data=${DATA_DIR} cfg:default.paths.logs=${LOG_DIR}
