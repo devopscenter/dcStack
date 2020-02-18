600fbee16c94e273419eb438a808f24d0856f195
Bob Lozano <boblozano@gmail.com>
2020-02-18 11:24:58 -0600

    0099ee: initial commit

---
+++ 0099ee-stack/web/web.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: web.sh
+#
+#         USAGE: web.sh
+#
+#   DESCRIPTION: install was is necessary for the web container
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#                Bob Lozano (), bob@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 11/21/2016 15:13:37
+#      REVISION:  ---
+#
+# Copyright 2014-2017 devops.center llc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#===============================================================================
+
+#set -o nounset     # Treat unset variables as an error
+set -o errexit      # exit immediately if command exits with a non-zero status
+#set -x             # essentially debug mode
+
+source /usr/local/bin/dcEnv.sh                       # initalize logging environment
+dcStartLog "install of app-specific web for 0099ff"
+
+sudo pip install -r requirements.txt
+sudo pip install --no-binary :all: -r requirements2.txt
+sudo pip install -r requirements3.txt
+
+# scratch volume
+sudo mkdir -p /media/data/tmp
+sudo chmod 777 -R /media/data/tmp
+
+dcEndLog "install of app-specific web for 0099ff"
