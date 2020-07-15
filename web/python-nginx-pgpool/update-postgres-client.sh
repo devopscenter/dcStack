21fec3c674fb27389a4cddafb0a48e19c977e6e6
Bob Lozano <boblozano@gmail.com>
2020-07-14 19:47:57 -0500

    lightweight scripts to update pg client and pgpool on instances

---
+++ web/python-nginx-pgpool/update-postgres-client.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: pgpool.sh
+#
+#         USAGE: pgpool.sh
+#
+#   DESCRIPTION: Install pgpool
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#                Bob Lozano (), bob@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 11/21/2016 15:13:37
+#      REVISION:  ---
+#
+# Copyright 2014-2017 devops.center llc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#===============================================================================
+
+#set -o nounset     # Treat unset variables as an error
+set -o errexit      # exit immediately if command exits with a non-zero status
+set -x             # essentially debug mode
+
+# Optionally over-ride default version of Postgres client
+
+if [[ -n "$1" ]]; then
+  POSTGRES_VERSION=$1
+fi
+
+# Add the PostgreSQL PGP key to verify their Debian packages.
+# It should be the same key as https://www.postgresql.org/media/keys/ACCC4CF8.asc
+sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+      sudo apt-key add -
+
+# Add PostgreSQL's repository. It contains the most recent stable release
+#     of PostgreSQL, ``9.5``.
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
+
+sudo sudo apt-get update
+
+sudo sudo apt-get -y -q install postgresql-client-${POSTGRES_VERSION} libpq5 libpq-dev
+
+
