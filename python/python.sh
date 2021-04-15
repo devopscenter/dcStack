73e8822a28abdb4a206b855bd7bacc5b6158ea10
Bob Lozano <boblozano@gmail.com>
2021-04-15 15:52:20 -0500

    python: trim more out of the installs when using the python3 that comes in the distro

--- python/python.sh
+++ python/python.sh
@@ -41,12 +41,6 @@ set -o verbose
 echo "============================ Building element: python ===================="
 echo "PATH=/usr/local/opt/python/bin:$PATH" | sudo tee -a /etc/environment
 
-sudo apt-get -qq update && apt-get -qq -y install python-software-properties software-properties-common && \
-    sudo add-apt-repository "deb http://gb.archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" && \
-    sudo apt-get -qq update
-
-sudo add-apt-repository -y ppa:saiarcot895/myppa
-
 export GIT_VERSION=2.31.1
 
 sudo apt-get -qq update
@@ -62,17 +56,6 @@ popd
 popd
 
 sudo apt-get -qq -y install sqlite3 libsqlite3-dev libssl-dev zlib1g-dev libxml2-dev libxslt-dev libbz2-dev gfortran libopenblas-dev liblapack-dev libatlas-dev subversion
-
-pushd /tmp
-sudo wget --quiet https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz -O /tmp/Python-${PYTHON_VERSION}.tgz
-sudo tar -xvf Python-${PYTHON_VERSION}.tgz
-pushd Python-${PYTHON_VERSION}
-sudo ./configure CFLAGSFORSHARED="-fPIC" CCSHARED="-fPIC" --quiet CCSHARED="-fPIC" --prefix=/usr/local/opt/python --exec-prefix=/usr/local/opt/python CCSHARED="-fPIC" \
-            && make clean && make --silent -j3 && sudo make --silent install
-popd
-
-which python && python --version
-
 sudo pip install -U setuptools-git wheel
 
 sudo mkdir -p /wheelhouse
