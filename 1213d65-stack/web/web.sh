2d39642c61461807ccb3a1d651fee24fc93c9d9b
Bob Lozano <boblozano@gmail.com>
2021-06-04 12:02:48 -0500

    1213d65: beginning of grafana / prometheus / postgres stack

---
+++ 1213d65-stack/web/web.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: web.sh
+#
+#         USAGE: web.sh
+#
+#   DESCRIPTION: install what is necessary for the web container
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gregg Jensen (), gjensen@devops.center
+#                Bob Lozano (), bob@devops.center
+#  ORGANIZATION: devops.center
+#       CREATED: 11/21/2016 15:13:37
+#      REVISION:  ---
+#
+# Copyright 2014-2017 devops.center llc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#===============================================================================
+
+#set -o nounset     # Treat unset variables as an error
+#set -o errexit      # exit immediately if command exits with a non-zero status
+#set -x             # essentially debug mode
+
+
+source /usr/local/bin/dcEnv.sh                       # initalize logging environment
+
+dcStartLog "install of app-specific web for 1213d64-stack (dcMonitoring)"
+
+# update 
+sudo apt-get update
+
+# add the grafana user
+useradd grafana
+
+# prerequisites 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y pdsh gunicorn \
+ python-dev python-flup python-ldap expect memcached \
+ sqlite3 libcairo2 libcairo2-dev libffi-dev librrd-dev python-cairo python-rrdtool 
+
+
+# install grafana helper tool wizzy to help with exporting dashboards
+npm install -g wizzy
+
+# install the python support libraries
+sudo -H pip install -r requirements.txt
+
+# set up the location for the files
+if [[ ! -d /opt ]]; then
+    sudo mkdir /opt
+    sudo chmod 777 /opt
+fi
+if [[ ! -d /usr/local/src ]]; then
+    sudo mkdir -p /usr/local/src
+    sudo chmod 777 /usr/local/src
+fi
+
+
+grafana_version=7.5.7
+
+
+# Install Grafana
+mkdir  -p /opt/grafana
+curl https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-${grafana_version}.linux-amd64.tar.gz -o /usr/local/src/grafana.tar.gz                                                                                  &&\
+tar -xzvf /usr/local/src/grafana.tar.gz -C /opt/grafana --strip-components=1
+rm /usr/local/src/grafana.tar.gz
+
+#-------------------------------------------------------------------------------
+# Configuration
+#-------------------------------------------------------------------------------
+
+# Configure Grafana
+cp conf/custom.ini /opt/grafana/conf/custom.ini
+
+# set up the supervisor start script for grafana
+sudo cp conf/supervisor-nginx.conf /etc/supervisor/conf.d/nginx.conf
+sudo cp conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+sudo cp conf/supervisor-grafana.conf /etc/supervisor/conf.d/grafana.conf
+sudo cp conf/run_grafana.sh /etc/supervisor/conf.d/run_grafana.sh
+sudo chmod a+x /etc/supervisor/conf.d/run_grafana.sh
+
+#
+# disable unused services
+#
+if [[ -f /etc/supervisor/conf.d/uwsgi.conf ]]; then
+    sudo mv /etc/supervisor/conf.d/uwsgi.conf /etc/supervisor/conf.d/uwsgi.save
+fi
+
+# cleanup
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# update the PATH variable in the bashrc with the needed new paths 
+echo "export PATH=/opt/grafana/bin:\$PATH" >> ~/.bashrc
+
+dcEndLog "install of app-specific web for 1213d64-stack (dcMonitoring)"
